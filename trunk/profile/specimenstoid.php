69bc61ac8ce4a0c90ebc01c0d762bc80bbcd9360
chicoreus <chicoreus@6bc6c840-c11a-4335-b936-cfadb8367715>
2013-06-25 18:06:15 +0000

    ISSUE:   PURPOSE: Provide list of specimens needing identification to the species level in a user's registered speciality.  DESCRIPTION:  First cut at display of a list of occurrence records not identified to the species level and matching an interest expressed in usertaxonomy.  Added a specimenstoid tab in the user profile, with supporting code in the PersonalSpecimenManager and ProfileManager classes.  Won't display anything unless there is at least one record for the currently logged in user in usertaxonomy.  Does not include means to enter data in usertaxonomy yet.  Tab in user profile may not work effectively for navigation. Current implementation of query on taxon tree expects interest to be on genus or family.

---
+++ trunk/profile/specimenstoid.php
@@ -0,0 +1,48 @@
+<?php
+include_once('../config/symbini.php');
+include_once($serverRoot.'/classes/PersonalSpecimenManager.php');
+@header("Content-Type: text/html; charset=".$charset);
+
+$collId = array_key_exists("collid",$_REQUEST)?$_REQUEST["collid"]:0;
+$formSubmit = array_key_exists("formsubmit",$_REQUEST)?$_REQUEST["formsubmit"]:"";
+
+$specHandler = new PersonalSpecimenManager();
+
+$occArr = array();
+if($symbUid){
+	$specHandler->setUid($symbUid);
+	$occArr = $specHandler->getOccurrenceArr();
+}
+
+$statusStr = '';
+?>
+<div style="margin:10px;">
+<?php 
+if($symbUid){
+	//Collection is defined and User is logged-in and have permissions
+	if($statusStr){
+		?>
+		<hr/>
+		<div style="margin:15px;color:red;">
+			<?php echo $statusStr; ?>
+		</div>
+		<hr/>
+		<?php 
+	}
+	$pendingIdents = array();
+	$pendingIdents = $specHandler->getSpecimensPendingIdent();
+	if (count($pendingIdents)==0) { 
+		echo "<h2>No specimens in your registered speciality need identification to the species level</h2>";
+	} else { 
+		echo "<h2>Specimens needing identification in your area of expertise.</h2>";
+	foreach ($pendingIdents as $key => $value) {
+		echo "<a href='../collections/editor/occurrenceeditor.php?occid=$key'>$value->sciname</a> $value->collectionCode $value->institutionCode $value->stateProvince<BR>";
+	}	
+	}
+	
+}
+else{
+	echo '<h2>Please <a href="../profile/index.php?&refurl='.$clientRoot.'/profile/personalspec.php?collid='.$collId.'">login</a></h2>';
+}
+?>	
+</div>
