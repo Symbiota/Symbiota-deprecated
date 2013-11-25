1b31958ea1a6e1c12b7e096c70b2f6172ffce402
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2013-11-25 07:09:36 +0000

    Fixed deprecated code on the Google map helpers

--- trunk/collections/mappointradius.php
+++ trunk/collections/mappointradius.php
@@ -11,70 +11,32 @@ $lngCenter = ($boundaryArr[1]>$boundaryArr[3]?((($boundaryArr[1]-$boundaryArr[3]
   <head>
     <title><?php echo $defaultTitle; ?> - Coordinate Mapper</title>
   </head> 
-  <body onload="initialize()"  onunload="GUnload()">
-    <script src="http://maps.google.com/maps?file=api&v=2&key=<?php echo $googleMapKey; ?>" type="text/javascript"></script>
+  <body>
+	<script src="http://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
     <script type="text/javascript">
-      //<![CDATA[
-      
-      	var zoomLevel = 5;
+		var map;
+		var marker;
       	
         function initialize(){
 
-            var map = new GMap2(document.getElementById("map"));
-            map.setCenter(new GLatLng(<?php echo $latCenter.','.$lngCenter; ?>), zoomLevel);
-            map.setUIToDefault();
-            
-            GEvent.addListener(map, 'dblclick', function(overlay, point) {
-				map.clearOverlays();
-				map.zoomIn(point,true);
-            });
+			var dmOptions = {
+				zoom: 5,
+				center: new google.maps.LatLng(<?php echo $latCenter.','.$lngCenter; ?>),
+				mapTypeId: google.maps.MapTypeId.TERRAIN,
+				scaleControl: true
+			};
 
-            GEvent.addListener(map, 'click', function(overlay, point) {
-				if(point) {
-					map.clearOverlays();
-                    var marker = new GMarker(point);
-                    map.addOverlay(marker);
-                    
-					// Add Coords by clicking the map
-    		        var latValue = point.y;
-    		        var lonValue = point.x;
-    		        latValue = latValue.toFixed(5);
-    		        lonValue = lonValue.toFixed(5);
-    				document.getElementById("latbox").value = latValue;
-                    document.getElementById("lonbox").value = lonValue;
-             	}
-            });
-        }
+	    	map = new google.maps.Map(document.getElementById("map"), dmOptions);
 
-        function mapClickRectangle(overlay,point){
-        	if(firstClick){   // First click
-            	map.clearOverlays();
-                r1 = point;
-                myrectangle = null;
-                firstClick = false;
-                eventOnMove = GEvent.addListener(map, 'mousemove',mapDragRectangle);
-				document.getElementById("nlat").value = "";
-				document.getElementById("slat").value = "";
-				document.getElementById("elon").value = "";
-				document.getElementById("wlon").value = "";
-            }
-            else{   // Second click
-				GEvent.removeListener(eventOnMove);
-				if(point){
-	            	map.clearOverlays();
-	                firstClick = true;
-				}
-				else{
-	                //Add Coords by clicking the map
-					var bounds = myrectangle.getBounds();
-	                var sw = bounds.getSouthWest();
-	                var ne = bounds.getNorthEast();
-					document.getElementById("nlat").value = ne.lat().toFixed(5);
-					document.getElementById("slat").value = sw.lat().toFixed(5);
-					document.getElementById("elon").value = ne.lng().toFixed(5);
-					document.getElementById("wlon").value = sw.lng().toFixed(5);
-				}
-            }
+			google.maps.event.addListener(map, 'click', function(event) {
+				if(marker) marker.setMap(null);
+				marker = new google.maps.Marker({
+					position: event.latLng,
+					map: map
+				});
+				document.getElementById("latbox").value = event.latLng.lat().toFixed(5);
+				document.getElementById("lonbox").value = event.latLng.lng().toFixed(5);
+			});
         }
 
         function updateParentForm() {
@@ -88,8 +50,9 @@ $lngCenter = ($boundaryArr[1]>$boundaryArr[3]?((($boundaryArr[1]-$boundaryArr[3]
             return false;
         }
 
-        //]]>
-    </script>
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+	</script>
     <div>Click once to capture coordinates.  
     Click on the Submit Coordinate button to transfer Coordinates. </div>
     <div id='map' style='width: 100%; height: 520px'></div>
