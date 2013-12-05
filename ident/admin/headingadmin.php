492167f03d37fee451e442f51a8bd2c43abd46b2
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2013-12-05 22:59:24 +0000

    Key character admin development

--- ident/admin/headingadmin.php
+++ ident/admin/headingadmin.php
@@ -4,22 +4,33 @@ include_once($serverRoot.'/classes/IdentCharAdmin.php');
 
 if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../ident/admin/headingadmin.php?'.$_SERVER['QUERY_STRING']);
 
-$action = array_key_exists('action',$_GET)?$_GET['action']:'';
 $hid = array_key_exists('hid',$_REQUEST)?$_REQUEST['hid']:0;
-$lang = array_key_exists('lang',$_REQUEST)?$_REQUEST['lang']:'';
+$action = array_key_exists('action',$_GET)?$_GET['action']:'';
+$langId = array_key_exists('langid',$_REQUEST)?$_REQUEST['langid']:'';
 
 $charManager = new IdentCharAdmin();
-
-$headingArr = array();
-if(!$hid) $headingArr = $charManager->getHeadingList();
+$charManager->setLangId($langId);
+$headingArr = $charManager->getHeadingArr();
 
 $isEditor = false;
 if($isAdmin || array_key_exists("KeyAdmin",$userRights)){
 	$isEditor = true;
 }
 
+$statusStr = '';
+if($isEditor && $action){
+	if($action == 'Create'){
+		$statusStr = $charManager->addingHeading($_POST['headingname'],$_POST['notes'],$_POST['sortsequence']);
+	}
+	elseif($action == 'Save'){
+		$statusStr = $charManager->editHeading($hid,$_POST['headingname'],$_POST['notes'],$_POST['sortsequence']);
+	}
+	elseif($action == 'Delete'){
+		$statusStr = $charManager->deleteHeading($hid);
+	}
+}
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<!DOCTYPE HTML>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset;?>">
@@ -39,6 +50,15 @@ if($isAdmin || array_key_exists("KeyAdmin",$userRights)){
 	<!-- This is inner text! -->
 	<div id="innertext">
 		<?php 
+		if($statusStr){
+			?>
+			<hr/>
+			<div style="margin:15px;color:<?php echo (strpos($statusStr,'SUCCESS')===0?'green':'red'); ?>;">
+				<?php echo $statusStr; ?>
+			</div>
+			<hr/>
+			<?php 
+		}
 		if($isEditor){
 			?>
 			<div style="float:right;margin:10px;">
@@ -46,23 +66,13 @@ if($isAdmin || array_key_exists("KeyAdmin",$userRights)){
 					<img src="../../images/add.png" alt="Create New Heading" />
 				</a>
 			</div>
-			<div id="addheadingdiv" style="display:none;">
+			<div id="addheadingdiv" style="display:<?php echo ($hid?'none':'block'); ?>;">
 				<form name="newheadingform" action="headingadmin.php" method="post" onsubmit="return validateNewHeadingForm(this)">
 					<fieldset>
 						<legend><b>New Heading</b></legend>
 						<div>
 							Heading Name<br />
 							<input type="text" name="headingname" maxlength="255" style="width:400px;" />
-						</div>
-						<div style="padding-top:6px;">
-							<b>Language</b><br />
-							<select name="langid">
-								<?php 
-								$langArr = $charManager->
-								
-								?>
-							</select>
-							<input type="text" name="language" />
 						</div>
 						<div style="padding-top:6px;">
 							<b>Notes</b><br />
@@ -73,7 +83,7 @@ if($isAdmin || array_key_exists("KeyAdmin",$userRights)){
 							<input type="text" name="sortsequence" />
 						</div>
 						<div style="width:100%;padding-top:6px;">
-							<button name="formsubmit" type="submit" value="Create">Create</button>
+							<button name="action" type="submit" value="Create">Create Heading</button>
 						</div>
 					</fieldset>
 				</form>
@@ -83,29 +93,55 @@ if($isAdmin || array_key_exists("KeyAdmin",$userRights)){
 				if($charArr){
 					?>
 					<h3>Characters by Heading</h3>
-					<ul>
-						<?php 
-						foreach($charArr as $k => $charList){
-							?>
-							<li>
-								<a href="#" onclick="toggle('char-<?php echo $k; ?>');"><?php echo $headingArr[$k]; ?></a>
-								<div id="char-<?php echo $k; ?>" style="display:block;">
-									<ul>
-										<?php 
-										foreach($charList as $cid => $charName){
-											echo '<li>';
-											echo '<a href="chardetails.php?cid='.$cid.'">'.$charName.'</a>';
-											echo '</li>';
-										}
-										?>
-									</ul>
-								</div>
-							</li>
-							<?php 
-						}
+					<?php 
+					foreach($charArr as $headingId => $charList){
 						?>
-					</ul>
-				<?php 
+						<div>
+							<a href="#" onclick="toggle('heading-<?php echo $headingId; ?>');"><?php echo $headingArr[$headingId]['name']; ?></a>
+							<a href="#" onclick="toggle('headingedit-<?php echo $headingId; ?>');"><img src="../../images/edit.png" /></a>
+							<div id="headingedit-<?php echo $headingId; ?>">
+								<fieldset>
+									<legend>Heading Editor</legend>
+									<form name="headingeditform" action="headingadmin.php" method="post">
+										<div style="margin:2px;">
+											<input name="headingname" type="text" value="<?php echo $headingArr[$headingId]['name']; ?>" />
+										</div>
+										<div style="margin:2px;">
+											<input name="notes" type="text" value="<?php echo $headingArr[$headingId]['notes']; ?>" />
+										</div>
+										<div style="margin:2px;">
+											<input name="sortsequence" type="text" value="<?php echo $headingArr[$headingId]['sortsequence']; ?>" />
+										</div>
+										<div>
+											<input name="hid" type="hidden" value="<?php echo $headingId; ?>" />
+											<button name="action" type="submit" value="Save">Save Edits</button>
+										</div>
+									</form>
+								</fieldset>
+								<fieldset>
+									<legend>Delete Heading</legend>
+									<form name="headingdeleteform" action="headingadmin.php" method="post">
+										<input name="hid" type="hidden" value="<?php echo $headingId; ?>" />
+										<button name="action" type="submit" value="Delete">Delete Heading</button>
+									</form>
+								</fieldset>
+							</div>
+							<div id="heading-<?php echo $headingId; ?>" style="display:none;">
+								<?php 
+								foreach($charList as $cid => $charName){
+									?>
+									<ul>
+										<li style="margin-left:10px;">
+											<?php echo '<a href="chardetails.php?cid='.$cid.'" target="_blank">'.$charName.'</a>'; ?>
+										</li>
+									</ul>
+									<?php 
+								}
+								?>
+							</div>
+						</div>
+						<?php 
+					}
 				}
 				else{
 					echo '<div style="font-weight:bold;font-size:120%;">There are no existing characters</div>';
