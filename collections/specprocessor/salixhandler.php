977916ee3f7e454a42cfddc1052c3c1d57d38f30
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-02-19 17:55:51 +0000

    Added modules for generating Word Clouds using OCR content for unprocessed specimens

---
+++ collections/specprocessor/salixhandler.php
@@ -0,0 +1,9 @@
+<?php
+include_once('../../config/symbini.php');
+include_once($serverRoot.'/classes/SalixUtilities.php');
+header("Content-Type: text/html; charset=".$charset);
+
+$salixhanlder = new SalixUtilities();
+$salixhanlder->buildWordStats();
+
+?>
---
+++ collections/specprocessor/wordcloudhandler.php
@@ -0,0 +1,33 @@
+<?php
+include_once('../../config/symbini.php');
+include_once($serverRoot.'/classes/WordCloud.php');
+header("Content-Type: text/html; charset=".$charset);
+
+$collTarget = array_key_exists("colltarget",$_REQUEST)?$_REQUEST["colltarget"]:5;
+
+$cloudHandler = new WordCloud();
+$cloudHandler->setWidth(800);
+$cloudHandler->buildWordFile($collTarget);
+
+?>
+<!doctype html>
+<html>
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset;?>">
+		<title><?php echo $defaultTitle; ?> - Word Cloud Handler Collections</title>
+	    <link type="text/css" href="../../css/main.css" rel="stylesheet" />
+		<script language="javascript" type="text/javascript">
+		</script>
+	</head>
+	<body>
+		<!-- This is inner text! -->
+		<div id="innertext">
+			<?php
+			$cloudPath = $clientRoot;
+			if(substr($cloudPath,-1) != '/' && substr($cloudPath,-1) != "\\") $cloudPath .= '/';
+			$cloudPath = 'temp/wordclouds/ocrcloud'.$collTarget.'.html';
+			echo '<a href="'.$cloudPath.'">Cloud'.$collTarget.'</a>';
+			?>
+		</div>
+	</body>
+</html>
