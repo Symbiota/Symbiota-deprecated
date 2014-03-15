5c604d2d822c704ecb776f915fde52e0265f4f4b
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-03-15 22:54:10 +0000

    ISSUE: redirect LBCC OCR NLP action to SpecProcNlpParserLBCCCommon, rather than SpecProcNlpParserLBCC

--- collections/editor/rpc/nlplbcc.php
+++ collections/editor/rpc/nlplbcc.php
@@ -1,11 +1,11 @@
 <?php
 include_once('../../../config/symbini.php');
-include_once($serverRoot.'/classes/SpecProcNlpParserLBCC.php');
+include_once($serverRoot.'/classes/SpecProcNlpParserLBCCCommon.php');
 header("Content-Type: text/html; charset=UTF-8");
-	
+
 $rawOcr = $_REQUEST['rawocr'];
-	
-$nlpManager = new SpecProcNlpParserLBCC();
+
+$nlpManager = new SpecProcNlpParserLBCCCommon();
 $dcObj = $nlpManager->parseTextBlock($rawOcr);
 
 echo $dcObj;
