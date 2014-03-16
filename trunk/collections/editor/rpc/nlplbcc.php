40a51c7c65a001c4b6f867ce169ed459d379c5fd
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-03-16 01:56:11 +0000

    PURPOSE: NLP adjustments, allow nlplbcc.php code to parse OCR when passed the ocr block primary key (prlid)

--- trunk/collections/editor/rpc/nlplbcc.php
+++ trunk/collections/editor/rpc/nlplbcc.php
@@ -1,12 +1,21 @@
 <?php
 include_once('../../../config/symbini.php');
-include_once($serverRoot.'/classes/SpecProcNlpParserLBCCCommon.php');
+//include_once($serverRoot.'/classes/SpecProcNlpParserLBCCCommon.php');
+include_once($serverRoot.'/classes/SpecProcNlpParserLBCC.php');
 header("Content-Type: text/html; charset=UTF-8");
 
-$rawOcr = $_REQUEST['rawocr'];
+$rawOcr = (array_key_exists('rawocr',$_REQUEST)?$_REQUEST['rawocr']:'');
+$prlid = (array_key_exists('prlid',$_REQUEST)?$_REQUEST['prlid']:'');
 
-$nlpManager = new SpecProcNlpParserLBCCCommon();
-$dcObj = $nlpManager->parseTextBlock($rawOcr);
+//$nlpManager = new SpecProcNlpParserLBCCCommon();
+$nlpManager = new SpecProcNlpParserLBCC();
+$dcObj = '';
+if($prlid){
+	$dcObj = $nlpManager->parseRawOcrRecord($prlid);
+}
+elseif($rawOcr){
+	$dcObj = $nlpManager->parseTextBlock($rawOcr);
+}
 
 echo $dcObj;
 ?>
