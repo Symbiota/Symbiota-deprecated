d315e879bdbe6773203165e5641f832af461a05b
rjanglin <rjanglin@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-03-26 17:10:08 +0000

    nlplbcc.php: included SpecProcNlp.php so that the nlpManager could be SpecProcNlp.  Added code to retrieve the url, collid and catalogNumber from the request and pass them to SpecProcNlp.

--- collections/editor/rpc/nlplbcc.php
+++ collections/editor/rpc/nlplbcc.php
@@ -1,20 +1,21 @@
 <?php
 include_once('../../../config/symbini.php');
-//include_once($serverRoot.'/classes/SpecProcNlpParserLBCCCommon.php');
-include_once($serverRoot.'/classes/SpecProcNlpParserLBCC.php');
+include_once($serverRoot.'/classes/SpecProcNlp.php');
 header("Content-Type: text/html; charset=UTF-8");
 
 $rawOcr = (array_key_exists('rawocr',$_REQUEST)?$_REQUEST['rawocr']:'');
 $prlid = (array_key_exists('prlid',$_REQUEST)?$_REQUEST['prlid']:'');
+$rl = $_REQUEST['iurl'];
+$collid = $_REQUEST['collid'];
+$catNo = $_REQUEST['catalognumber'];
 
-//$nlpManager = new SpecProcNlpParserLBCCCommon();
-$nlpManager = new SpecProcNlpParserLBCC();
+$nlpManager = new SpecProcNlp();
 $dcObj = '';
 if($prlid){
 	$dcObj = $nlpManager->parseRawOcrRecord($prlid);
 }
 elseif($rawOcr){
-	$dcObj = $nlpManager->parseTextBlock($rawOcr);
+	$dcObj = $nlpManager->parseTextBlock($rawOcr, $collid, $rl, $catNo);
 }
 
 echo $dcObj;
