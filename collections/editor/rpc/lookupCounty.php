36f2cd1bd764e8c58bcc744a2178b2b292769df8
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-05-16 21:53:36 +0000

    ISSUE: Convert latin data within the lookup tables to UTF-8 so that AJAX JQuery autocomplete functions work properly (Only concerns portals with data stored in a latin character set)

--- collections/editor/rpc/lookupCounty.php
+++ collections/editor/rpc/lookupCounty.php
@@ -1,29 +1,35 @@
 <?php
+include_once('../../../config/symbini.php');
+include_once('../../../config/dbconnection.php');
+$con = MySQLiConnectionFactory::getCon("readonly");
+$retArr = Array();
+$queryString = $con->real_escape_string($_REQUEST['term']);
+$stateStr = array_key_exists('state',$_REQUEST)?$con->real_escape_string($_REQUEST['state']):'';
 
- 	include_once('../../../config/dbconnection.php');
-	$con = MySQLiConnectionFactory::getCon("readonly");
-	$retArr = Array();
-	$queryString = $con->real_escape_string($_REQUEST['term']);
-	$stateStr = array_key_exists('state',$_REQUEST)?$con->real_escape_string($_REQUEST['state']):'';
-
-	$sql = 'SELECT DISTINCT c.countyname FROM lkupcounty c ';
-	$sqlWhere = 'WHERE c.countyname LIKE "'.$queryString.'%" ';
-	if($stateStr){
-		$sql .= 'INNER JOIN lkupstateprovince s ON c.stateid = s.stateid ';
-		$sqlWhere .= 'AND s.statename = "'.$stateStr.'" ';
+$sql = 'SELECT DISTINCT c.countyname FROM lkupcounty c ';
+$sqlWhere = 'WHERE c.countyname LIKE "'.$queryString.'%" ';
+if($stateStr){
+	$sql .= 'INNER JOIN lkupstateprovince s ON c.stateid = s.stateid ';
+	$sqlWhere .= 'AND s.statename = "'.$stateStr.'" ';
+}
+$sql .= $sqlWhere.'ORDER BY c.countyname';
+//echo $sql;
+$result = $con->query($sql);
+while ($row = $result->fetch_object()) {
+	$countyStr = $row->countyname;
+	if($charset == 'ISO-8859-1'){
+		if(mb_detect_encoding($countyStr,'UTF-8,ISO-8859-1',true) == "ISO-8859-1"){
+			$countyStr = utf8_encode($countyStr);
+		}
 	}
-	$sql .= $sqlWhere.'ORDER BY c.countyname';
-	//echo $sql;
-	$result = $con->query($sql);
-	while ($row = $result->fetch_object()) {
-		$retArr[] = $row->countyname;
-	}
-	$result->close();
-	$con->close();
-	if($retArr){
-		echo '["'.implode('","',($retArr)).'"]';
-	}
-	else{
-		echo '';
-	}
+	$retArr[] = $countyStr;
+}
+$result->free();
+$con->close();
+if($retArr){
+	echo '["'.implode('","',($retArr)).'"]';
+}
+else{
+	echo '';
+}
 ?>
