dd0054bdb33a31b26dfa212802ccb97bb8a696f0
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-05-16 21:53:36 +0000

    ISSUE: Convert latin data within the lookup tables to UTF-8 so that AJAX JQuery autocomplete functions work properly (Only concerns portals with data stored in a latin character set)

--- trunk/collections/editor/rpc/lookupState.php
+++ trunk/collections/editor/rpc/lookupState.php
@@ -1,29 +1,35 @@
 <?php
+include_once('../../../config/symbini.php');
+include_once('../../../config/dbconnection.php');
+$con = MySQLiConnectionFactory::getCon("readonly");
+$retArr = Array();
+$queryString = $con->real_escape_string($_REQUEST['term']);
+$countryStr = array_key_exists('country',$_REQUEST)?$con->real_escape_string($_REQUEST['country']):'';
 
-	include_once('../../../config/dbconnection.php');
-	$con = MySQLiConnectionFactory::getCon("readonly");
-	$retArr = Array();
-	$queryString = $con->real_escape_string($_REQUEST['term']);
-	$countryStr = array_key_exists('country',$_REQUEST)?$con->real_escape_string($_REQUEST['country']):'';
-
-	$sql = 'SELECT DISTINCT s.statename FROM lkupstateprovince s ';
-	$sqlWhere = 'WHERE s.statename LIKE "'.$queryString.'%" ';
-	if($countryStr){
-		$sql .= 'INNER JOIN lkupcountry c ON s.countryid = c.countryid ';
-		$sqlWhere .= 'AND c.countryname = "'.$countryStr.'" ';
+$sql = 'SELECT DISTINCT s.statename FROM lkupstateprovince s ';
+$sqlWhere = 'WHERE s.statename LIKE "'.$queryString.'%" ';
+if($countryStr){
+	$sql .= 'INNER JOIN lkupcountry c ON s.countryid = c.countryid ';
+	$sqlWhere .= 'AND c.countryname = "'.$countryStr.'" ';
+}
+$sql .= $sqlWhere.'ORDER BY s.statename';
+//echo $sql;
+$result = $con->query($sql);
+while ($row = $result->fetch_object()) {
+	$stateStr = $row->statename;
+	if($charset == 'ISO-8859-1'){
+		if(mb_detect_encoding($stateStr,'UTF-8,ISO-8859-1',true) == "ISO-8859-1"){
+			$stateStr = utf8_encode($stateStr);
+		}
 	}
-	$sql .= $sqlWhere.'ORDER BY s.statename';
-	//echo $sql;
-	$result = $con->query($sql);
-	while ($row = $result->fetch_object()) {
-		$retArr[] = $row->statename;
-	}
-	$result->close();
-	$con->close();
-	if($retArr){
-		echo '["'.implode('","',($retArr)).'"]';
-	}
-	else{
-		echo '';
-	}
+	$retArr[] = $stateStr;
+}
+$result->close();
+$con->close();
+if($retArr){
+	echo '["'.implode('","',($retArr)).'"]';
+}
+else{
+	echo '';
+}
 ?>
