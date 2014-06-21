024b5cb58774966b6fef4587f03cc1197fa53dd4
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-06-21 15:46:49 +0000

    ISSUE: wrapped new schema patch into the stored procedure wrapper established for the 0.9.1.13 patch

--- config/schema/utf8/db_schema_patch-0.9.1.14.sql
+++ config/schema/utf8/db_schema_patch-0.9.1.14.sql
@@ -1,5 +1,43 @@
-#Track schema patch history
-INSERT INTO schemaversion (versionnumber) values ('0.9.1.14');
+DROP PROCEDURE updateSymbiotaSchema;
+
+DELIMITER |
+
+CREATE PROCEDURE updateSymbiotaSchema ()
+
+BEGIN
+  DECLARE requiredVersion varchar(20);  -- version needed for update to fire
+  DECLARE currentVersion varchar(20);   -- version present in schema
+  DECLARE newVersion varchar(20);       -- version this update will apply
+  DECLARE okToUpdate boolean DEFAULT FALSE;
+  DECLARE done boolean DEFAULT FALSE;
+  DECLARE curVersion CURSOR for select versionnumber from schemaversion order by dateapplied desc limit 1;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  --  (1)  Change the version numbers ****************************
+  --  Previous version must be this version for the update to fire
+  SET requiredVersion = '0.9.1.13';
+  SET newVersion = '0.9.1.14';
+  --  ************************************************************
+
+  OPEN curVersion;
+  
+  verLoop: LOOP
+     FETCH curVersion into currentVersion;
+     IF done THEN
+        LEAVE verLoop;
+     END IF;
+     IF currentVersion = requiredVersion THEN 
+        SET okToUpdate = TRUE;
+     END IF;
+  END LOOP;
+
+IF okToUpdate THEN 
+
+START TRANSACTION;
+
+INSERT INTO schemaversion (versionnumber) values (newVersion);
+
+-- (2) ******** Schema Changes to be applied in this update *********************
 
 #GUID adjustments
 ALTER TABLE `guidoccurrences` 
@@ -806,3 +844,22 @@ CREATE TABLE `taxaprofilepubmaplink` (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--  ******* End of Schema Changes to be applied in this update 
+
+COMMIT;
+
+--  if in MySQL/MARIADB 5.2+ where SIGNAL is supported, can return an error condition
+--  ELSE
+   -- SIGNAL SQLSTATE VALUE '99999'
+   --   SET MESSAGE_TEXT = 'Prerequisite schema version not found ' ;
+END IF; 
+
+END|
+
+DELIMITER ;
+
+CALL updateSymbiotaSchema();
+
+DROP PROCEDURE updateSymbiotaSchema;
+
