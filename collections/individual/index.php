605eb9424320fe926021fd10c50326154372dc5a
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-06-24 00:16:48 +0000

    ISSUE: Added code to the specimen details page to force a browser cache refresh

--- collections/individual/index.php
+++ collections/individual/index.php
@@ -2,6 +2,8 @@
 include_once('../../config/symbini.php');
 include_once($serverRoot.'/classes/OccurrenceIndividualManager.php');
 header("Content-Type: text/html; charset=".$charset);
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
 $occid = array_key_exists("occid",$_REQUEST)?trim($_REQUEST["occid"]):0;
 $collId = array_key_exists("collid",$_REQUEST)?trim($_REQUEST["collid"]):0;
