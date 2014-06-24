ea189e0fcc7f097d3bda9119ce4f543d637e6648
bdbrandt <bdbrandt@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-06-24 16:18:07 +0000

    Update to OOTD game code that allows for multiple games to run at once.

--- trunk/classes/GamesManager.php
+++ trunk/classes/GamesManager.php
@@ -81,11 +81,11 @@ class GamesManager {
 		return $retArr;
 	}
 	
-	public function setOOTD($clid){
+	public function setOOTD($oodID,$clid){
 		$currentDate = date("Y-m-d");
 		$replace = 0;
-		if(file_exists('../../temp/ootd/info.json')){
-			$oldArr = json_decode(file_get_contents('../../temp/ootd/info.json'), true);
+		if(file_exists('../../temp/ootd/'.$oodID.'_info.json')){
+			$oldArr = json_decode(file_get_contents('../../temp/ootd/'.$oodID.'_info.json'), true);
 			$lastDate = $oldArr['lastDate'];
 			$lastCLID = $oldArr['clid'];
 			if(($currentDate > $lastDate) || ($clid != $lastCLID)){
@@ -99,27 +99,27 @@ class GamesManager {
 		if($replace == 1){
 			//Delete old files
 			$previous = Array();
-			if(file_exists('../../temp/ootd/previous.json')){
-				$previous = json_decode(file_get_contents('../../temp/ootd/previous.json'), true);
-				unlink('../../temp/ootd/previous.json');
+			if(file_exists('../../temp/ootd/'.$oodID.'_previous.json')){
+				$previous = json_decode(file_get_contents('../../temp/ootd/'.$oodID.'_previous.json'), true);
+				unlink('../../temp/ootd/'.$oodID.'_previous.json');
 			}
-			if(file_exists('../../temp/ootd/info.json')){
-				unlink('../../temp/ootd/info.json');
+			if(file_exists('../../temp/ootd/'.$oodID.'_info.json')){
+				unlink('../../temp/ootd/'.$oodID.'_info.json');
 			}
-			if(file_exists('../../temp/ootd/organism300_1.jpg')){
-				unlink('../../temp/ootd/organism300_1.jpg');
+			if(file_exists('../../temp/ootd/'.$oodID.'_organism300_1.jpg')){
+				unlink('../../temp/ootd/'.$oodID.'_organism300_1.jpg');
 			}
-			if(file_exists('../../temp/ootd/organism300_2.jpg')){
-				unlink('../../temp/ootd/organism300_2.jpg');
+			if(file_exists('../../temp/ootd/'.$oodID.'_organism300_2.jpg')){
+				unlink('../../temp/ootd/'.$oodID.'_organism300_2.jpg');
 			}
-			if(file_exists('../../temp/ootd/organism300_3.jpg')){
-				unlink('../../temp/ootd/organism300_3.jpg');
+			if(file_exists('../../temp/ootd/'.$oodID.'_organism300_3.jpg')){
+				unlink('../../temp/ootd/'.$oodID.'_organism300_3.jpg');
 			}
-			if(file_exists('../../temp/ootd/organism300_4.jpg')){
-				unlink('../../temp/ootd/organism300_4.jpg');
+			if(file_exists('../../temp/ootd/'.$oodID.'_organism300_4.jpg')){
+				unlink('../../temp/ootd/'.$oodID.'_organism300_4.jpg');
 			}
-			if(file_exists('../../temp/ootd/organism300_5.jpg')){
-				unlink('../../temp/ootd/organism300_5.jpg');
+			if(file_exists('../../temp/ootd/'.$oodID.'_organism300_5.jpg')){
+				unlink('../../temp/ootd/'.$oodID.'_organism300_5.jpg');
 			}
 			
 			//Create new files
@@ -150,7 +150,7 @@ class GamesManager {
 			$sql2 = 'SELECT t.TID, t.SciName, t.UnitName1, s.family '.
 				'FROM taxa AS t INNER JOIN taxstatus AS s ON t.TID = s.tid '.
 				'WHERE s.taxauthid = 1 AND t.TID = '.$randTaxa.' ';
-			//echo '<div>'.$sql.'</div>';
+			//echo '<div>'.$sql2.'</div>';
 			$rs = $this->conn->query($sql2);
 			while($row = $rs->fetch_object()){
 				$ootdInfo['tid'] = $row->TID;
@@ -188,7 +188,7 @@ class GamesManager {
 				else{
 					$file = $row->url;
 				}
-				$newfile = '../../temp/ootd/organism300_'.$cnt.'.jpg';
+				$newfile = '../../temp/ootd/'.$oodID.'_organism300_'.$cnt.'.jpg';
 				if(fopen($file, "r")){
 					copy($file, $newfile);
 					$files[] = $newfile;
@@ -199,25 +199,25 @@ class GamesManager {
 			$ootdInfo['images'] = $files;
 			
 			if(array_diff($tidArr,$previous)){
-				$fp = fopen('../../temp/ootd/previous.json', 'w');
+				$fp = fopen('../../temp/ootd/'.$oodID.'_previous.json', 'w');
 				fwrite($fp, json_encode($previous));
 				fclose($fp);
 			}
-			$fp = fopen('../../temp/ootd/info.json', 'w');
+			$fp = fopen('../../temp/ootd/'.$oodID.'_info.json', 'w');
 			fwrite($fp, json_encode($ootdInfo));
 			fclose($fp);
 		}
 		
 		
 		
-		$infoArr = json_decode(file_get_contents('../../temp/ootd/info.json'), true);
+		$infoArr = json_decode(file_get_contents('../../temp/ootd/'.$oodID.'_info.json'), true);
 		//echo json_encode($infoArr);
 		return $infoArr;
 	}
 	
-	public function getAnswer(){
+	/*public function getAnswer(){
 		$answer = json_decode(file_get_contents('../../temp/ootd/info.json'), true);
 		return $answer;
-	}
+	}*/
 }
 ?>
--- trunk/games/ootd/readme.txt
+++ trunk/games/ootd/readme.txt
@@ -6,8 +6,11 @@ The first time you add block to a page, click on "Click here to test your knowle
 link to activate game and establish images for the first time. 
 
 <?php
+//If more than one game will be active, assign unique numerical ids for each game.
+//If only one game will be active, leave set to 1. 
+$oodID = 1; 
 //Enter checklist id (clid) of the checklist you wish to use 
-$ootdGameChecklist = 1; 
+$ootdGameChecklist = 1;
 //Change to modify title
 $ootdGameTitle = "Organism of the Day "; 
 //Replace "plant" with the type of organism, eg: plant, animal, insect, fungi, etc.
@@ -17,11 +20,11 @@ $ootdGameType = "plant";
 	<div style="font-size:130%;font-weight:bold;">
 		<?php echo $ootdGameTitle; ?>
 	</div>
-	<a href="<?php echo $clientRoot; ?>/games/ootd/index.php?cl=<?php echo $ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-		<img src="<?php echo $clientRoot; ?>/temp/ootd/organism300_1.jpg" style="width:250px;border:0px;" />
+	<a href="<?php echo $clientRoot; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+		<img src="<?php echo $clientRoot; ?>/temp/ootd/<?php echo $oodID; ?>_organism300_1.jpg" style="width:250px;border:0px;" />
 	</a><br/>
 	<b>What is this <?php echo $ootdGameType; ?>?</b><br/>
-	<a href="<?php echo $clientRoot; ?>/games/ootd/index.php?cl=<?php echo $ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+	<a href="<?php echo $clientRoot; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
 		Click here to test your knowledge
 	</a>
 </div>
