e8db9f3dcc2cf9e1e0b20c8b0916ee385e02c738
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-12-07 06:55:59 +0000

    - Reworked the SALIX word stat tables to create word stats for the whole portal collection rather than different stats for each collection. Also improved the efficiency.

--- trunk/collections/specprocessor/salix/salixhandler.php
+++ trunk/collections/specprocessor/salix/salixhandler.php
@@ -2,11 +2,29 @@
 include_once('../../../config/symbini.php');
 include_once($serverRoot.'/classes/SalixUtilities.php');
 header("Content-Type: text/html; charset=".$charset);
+if(!$SYMB_UID){
+	header('Location: ../../../profile/index.php?refurl=../collections/specprocessor/salix/salixhandler.php?'.$_SERVER['QUERY_STRING']);
+}
 
-$collStr = (isset($_REQUEST['collstr'])?$_REQUEST['collstr']:'');
+$action = (isset($_REQUEST['action'])?$_REQUEST['action']:'');
+$verbose = (isset($_REQUEST['verbose'])?$_REQUEST['verbose']:1);
 
-$salixhanlder = new SalixUtilities();
-echo '<ul>';
-$salixhanlder->batchWordStats($collStr);
-echo '</ul>';
+$isEditor = 0;
+if($SYMB_UID){
+	if($isAdmin){
+		$isEditor = 1;
+	}
+	else{
+		if(array_key_exists("CollAdmin",$userRights)){
+			$isEditor = 1;
+		}
+	}
+}
+if($isEditor){
+	if($action == ''){
+		$salixHanlder = new SalixUtilities();
+		$salixHanlder->setVerbose($verbose);
+		$salixHanlder->buildWordStats();
+	}
+}
 ?>
