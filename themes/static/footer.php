6439df9c0b695b8b914668ac55dd94b6bdabd1b2
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2014-12-18 07:30:17 +0000

    - Infrastructure for providing design layout themes

---
+++ themes/static/footer.php
@@ -0,0 +1,13 @@
+    	</td>
+	    <td class="middleright" valign="top">
+	        <!-- this is the far right section -->
+	        <img src="<?php echo $clientRoot; ?>/images/layout/defaultrightstrip.gif">
+	    </td>
+	</tr>
+	<tr>
+		<td class="footer" colspan="3">
+	        <!-- this is the bottom section -->
+			<img src="<?php echo $clientRoot; ?>/images/layout/defaultfooter.gif" border="0"/>
+		</td>
+	</tr>
+</table>
---
+++ themes/static/readme.txt
@@ -0,0 +1,10 @@
+Description: Static wide that doesn't change when browser size is modified. These websites typically have a single header graphic that has a width close to the width of the website. Left or top menus can be used
+
+Design layout files:
+	/header.php 
+	/leftmenu.php
+	/footer.php
+	/index.php
+	/css/main.css		
+	/css/speciesprofile.css
+
