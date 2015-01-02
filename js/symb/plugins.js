2aba03e00ff2ae01d7f8ebc142f09a1f7cc3ebfc
bdbrandt <bdbrandt@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-01-02 17:32:28 +0000

    Updates to the slideshow plugin that allow image captions to be toggled and for the time interval between image rotations to be set in the plugin settings. Also fix for portrait images to be vertically centered within slideshow window.

---
+++ js/symb/plugins.js
@@ -0,0 +1,25 @@
+function toggle(target){
+	var objDiv = document.getElementById(target);
+	if(objDiv){
+		if(objDiv.style.display=="none"){
+			objDiv.style.display = "block";
+		}
+		else{
+			objDiv.style.display = "none";
+		}
+	}
+	else{
+	  	var divs = document.getElementsByTagName("div");
+	  	for (var h = 0; h < divs.length; h++) {
+	  	var divObj = divs[h];
+			if(divObj.className == target){
+				if(divObj.style.display=="none"){
+					divObj.style.display="block";
+				}
+			 	else {
+			 		divObj.style.display="none";
+			 	}
+			}
+		}
+	}
+}
