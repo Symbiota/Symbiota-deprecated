98771f32a5cafe28af61d9c9940ff4d3abcea726
rjanglin <rjanglin@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-01-21 00:31:58 +0000

    Added otherCatalogNumbers to search

--- collections/harvestparams.php
+++ collections/harvestparams.php
@@ -4,18 +4,18 @@ include_once($serverRoot.'/classes/OccurrenceManager.php');
 header("Content-Type: text/html; charset=".$charset);
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
- 
-$collManager = new OccurrenceManager(); 
+
+$collManager = new OccurrenceManager();
 $collArray = $collManager->getSearchTerms();
 $collManager->reset();
 ?>
- 
+
 <html>
 <head>
     <title><?php echo $defaultTitle; ?> Collection Search Parameters</title>
 	<link href="../css/base.css" type="text/css" rel="stylesheet" />
 	<link href="../css/main.css" type="text/css" rel="stylesheet" />
-	<link href="../css/jquery-ui.css" type="text/css" rel="Stylesheet" />	
+	<link href="../css/jquery-ui.css" type="text/css" rel="Stylesheet" />
 	<script type="text/javascript" src="../js/jquery.js"></script>
 	<script type="text/javascript" src="../js/jquery-ui.js"></script>
 	<script type="text/javascript" src="../js/symb/collections.harvestparams.js?var=1303"></script>
@@ -36,11 +36,11 @@ $collManager->reset();
 	else{
 		?>
 		<div class='navpath'>
-			<a href='../index.php'>Home</a> &gt;&gt; 
-			<a href='index.php'>Collections</a> &gt;&gt; 
+			<a href='../index.php'>Home</a> &gt;&gt;
+			<a href='index.php'>Collections</a> &gt;&gt;
 			<b>Search Criteria</b>
 		</div>
-		<?php 
+		<?php
 	}
 	?>
 
@@ -52,7 +52,7 @@ $collManager->reset();
 			<div style="margin:10 0 10 0;"><hr></div>
 			<div style='float:right;margin:10px;'>
 				<input type="image" name="display1" id="display1" class="hoverHand" src='../images/search.gif'
-					onmouseover="javascript: this.src = '../images/search_rollover.gif';" 
+					onmouseover="javascript: this.src = '../images/search_rollover.gif';"
 					onmouseout="javascript: this.src = '../images/search.gif';"
 					title="Click button to display the results of your search">
 			</div>
@@ -68,7 +68,7 @@ $collManager->reset();
 						<option id='sciname' value='3' <?php if(array_key_exists("taxontype",$collArray) && $collArray["taxontype"] == "3") echo "SELECTED"; ?> >Scientific Name only</option>
 						<option id='highertaxon' value='4' <?php if(array_key_exists("taxontype",$collArray) && $collArray["taxontype"] == "4") echo "SELECTED"; ?> >Higher Taxonomy</option>
 						<option id='commonname' value='5' <?php if(array_key_exists("taxontype",$collArray) && $collArray["taxontype"] == "5") echo "SELECTED"; ?> >Common Name</option>
-					</select>: 
+					</select>:
 					<input id="taxa" type="text" size="60" name="taxa" value="<?php if(array_key_exists("taxa",$collArray)) echo $collArray["taxa"]; ?>" title="Separate multiple taxa w/ commas" />
 				</div>
 			</div>
@@ -89,7 +89,7 @@ $collManager->reset();
 				Locality: <input type="text" id="locality" size="43" name="local" value="<?php if(array_key_exists("local",$collArray)) echo $collArray["local"]; ?>" />
 			</div>
 			<div>
-				Elevation: <input type="text" id="elevlow" size="10" name="elevlow" value="<?php if(array_key_exists("elevlow",$collArray)) echo $collArray["elevlow"]; ?>" /> to  
+				Elevation: <input type="text" id="elevlow" size="10" name="elevlow" value="<?php if(array_key_exists("elevlow",$collArray)) echo $collArray["elevlow"]; ?>" /> to
 				<input type="text" id="elevhigh" size="10" name="elevhigh" value="<?php if(array_key_exists("elevhigh",$collArray)) echo $collArray["elevhigh"]; ?>" />
 			</div>
 			<div style="margin:10 0 10 0;">
@@ -98,9 +98,9 @@ $collManager->reset();
 			</div>
 			<div style="width:270px;float:left;border:2px solid brown;padding:10px;margin-bottom:10px;">
 				<div style="font-weight:bold;">
-					Bounding box coordinates in decimal degrees  
+					Bounding box coordinates in decimal degrees
 				</div>
-				<?php 
+				<?php
 					$upperLat = "";$bottomLat = "";$leftLong = "";$rightLong = "";
 					if(array_key_exists("llbound",$collArray)){
 						$llBoundArr = explode(";",$collArray["llbound"]);
@@ -109,10 +109,10 @@ $collManager->reset();
 						$leftLong = $llBoundArr[2];
 						$rightLong = $llBoundArr[3];
 					}
-				
+
 				?>
 				<div title="Northern hemisphere is positive; Southern is negative">
-					Northern Latitude: <input type="text" id="upperlat" name="upperlat" size="7" value="<?php echo $upperLat; ?>" onchange="javascript:checkUpperLat();" style="margin-left:9px;"> 
+					Northern Latitude: <input type="text" id="upperlat" name="upperlat" size="7" value="<?php echo $upperLat; ?>" onchange="javascript:checkUpperLat();" style="margin-left:9px;">
 					<select id="upperlat_NS" name="upperlat_NS" onchange="javascript:checkUpperLat();">
 						<option id="nlN" value="N">N</option>
 						<option id="nlS" value="S">S</option>
@@ -126,7 +126,7 @@ $collManager->reset();
 					</select>
 				</div>
 				<div title="Easterm hemisphere is positive; Western is negative">
-					Western Longitude: <input type="text" id="leftlong" name="leftlong" size="7" value="<?php echo $leftLong; ?>" onchange="javascript:checkLeftLong();"> 
+					Western Longitude: <input type="text" id="leftlong" name="leftlong" size="7" value="<?php echo $leftLong; ?>" onchange="javascript:checkLeftLong();">
 					<select id="leftlong_EW" name="leftlong_EW" onchange="javascript:checkLeftLong();">
 						<option id="llW" value="W">W</option>
 						<option id="llE" value="E">E</option>
@@ -145,9 +145,9 @@ $collManager->reset();
 			</div>
 			<div style="width:240px; float:left;border:2px solid brown;padding:10px;margin-left:10px;">
 				<div style="font-weight:bold;">
-					Point-Radius search  
+					Point-Radius search
 				</div>
-				<?php 
+				<?php
 					$pointLat = "";$pointLong = "";$radius = "";
 					if(array_key_exists("llpoint",$collArray)){
 						$llPointArr = explode(";",$collArray["llpoint"]);
@@ -155,7 +155,7 @@ $collManager->reset();
 						$pointLong = $llPointArr[1];
 						$radius = $llPointArr[2];
 					}
-				
+
 				?>
 				<div title="Northern hemisphere is positive; Southern is negative">
 					Latitude: <input type="text" id="pointlat" name="pointlat" size="7" value="<?php echo $pointLat; ?>" onchange="javascript:checkPointLat();" style="margin-left:11px;">
@@ -165,7 +165,7 @@ $collManager->reset();
 					</select>
 				</div>
 				<div title="Easterm hemisphere is positive; Western is negative">
-					Longitude: <input type="text" id="pointlong" name="pointlong" size="7" value="<?php echo $pointLong; ?>" onchange="javascript:checkPointLong();"> 
+					Longitude: <input type="text" id="pointlong" name="pointlong" size="7" value="<?php echo $pointLong; ?>" onchange="javascript:checkPointLong();">
 					<select id="pointlong_EW" name="pointlong_EW" onchange="javascript:checkPointLong();">
 						<option id="W" value="W">W</option>
 						<option id="E" value="E">E</option>
@@ -175,7 +175,7 @@ $collManager->reset();
 					<img src="../images/world40.gif" width="15px" title="Find Coordinate" />
 				</div>
 				<div>
-					Radius: <input type="text" id="radiustemp" name="radiustemp" size="5" value="<?php echo $radius; ?>" style="margin-left:15px;" onchange="updateRadius();"> 
+					Radius: <input type="text" id="radiustemp" name="radiustemp" size="5" value="<?php echo $radius; ?>" style="margin-left:15px;" onchange="updateRadius();">
 					<select id="radiusunits" name="radiusunits" onchange="updateRadius();">
 						<option value="km">Kilometers</option>
 						<option value="mi">Miles</option>
@@ -188,16 +188,16 @@ $collManager->reset();
 				<h1>Collector Criteria:</h1>
 			</div>
 			<div>
-				Collector's Last Name: 
+				Collector's Last Name:
 				<input type="text" id="collector" size="32" name="collector" value="<?php if(array_key_exists("collector",$collArray)) echo $collArray["collector"]; ?>" title="Separate multiple terms w/ commas" />
 			</div>
 			<div>
-				Collector's Number: 
+				Collector's Number:
 				<input type="text" id="collnum" size="31" name="collnum" value="<?php if(array_key_exists("collnum",$collArray)) echo $collArray["collnum"]; ?>" title="Separate multiple terms by commas and ranges by ' - ' (space before and after dash required), e.g.: 3542,3602,3700 - 3750" />
 			</div>
 			<div>
-				Collection Date: 
-				<input type="text" id="eventdate1" size="32" name="eventdate1" style="width:100px;" value="<?php if(array_key_exists("eventdate1",$collArray)) echo $collArray["eventdate1"]; ?>" title="Single date or start date of range" /> - 
+				Collection Date:
+				<input type="text" id="eventdate1" size="32" name="eventdate1" style="width:100px;" value="<?php if(array_key_exists("eventdate1",$collArray)) echo $collArray["eventdate1"]; ?>" title="Single date or start date of range" /> -
 				<input type="text" id="eventdate2" size="32" name="eventdate2" style="width:100px;" value="<?php if(array_key_exists("eventdate2",$collArray)) echo $collArray["eventdate2"]; ?>" title="End date of range; leave blank if searching for single date" />
 			</div>
 			<div>
@@ -207,7 +207,10 @@ $collManager->reset();
 				Catalog Number:
                 <input type="text" id="catnum" size="32" name="catnum" value="<?php if(array_key_exists("catnum",$collArray)) echo $collArray["catnum"]; ?>" title="Separate multiple terms w/ commas" />
 			</div>
-			<!-- 
+				Other CatalogNumbers:
+				<input type="text" id="othercatnum" size="32" name="othercatnum" value="<?php if(array_key_exists("othercatnum",$collArray)) echo $collArray["othercatnum"]; ?>" title="Separate multiple terms w/ commas" />
+			</div>
+			<!--
 			<div>
 				Type Status:
                 <input type="text" size="32" id="typestatus" name="typestatus" value="<?php if(array_key_exists("typestatus",$collArray)) echo $collArray["typestatus"]; ?>" title="Separate multiple terms w/ commas" />
@@ -219,8 +222,8 @@ $collManager->reset();
 					Click Here</a> for more information on how this query page works... -->
 			</div>
 			<div>
-				<input id="display2" name="display2" type="image" class="hoverHand" src='../images/search.gif' 
-					onmouseover="javascript:this.src = '../images/search_rollover.gif';" 
+				<input id="display2" name="display2" type="image" class="hoverHand" src='../images/search.gif'
+					onmouseover="javascript:this.src = '../images/search_rollover.gif';"
 					onmouseout="javascript:this.src = '../images/search.gif';"
 					title="Click button to display the results of your search">
 			</div>
