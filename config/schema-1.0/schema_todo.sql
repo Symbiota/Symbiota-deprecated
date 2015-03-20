5abe5606b8afadeb5ffa96f8cac0255bdd167335
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-03-20 15:08:11 +0000

    - Bug fix on schema version 1 that interfered with initial admin user not having SuperAdmin permissions

--- config/schema-1.0/schema_todo.sql
+++ config/schema-1.0/schema_todo.sql
@@ -5,6 +5,10 @@ ALTER TABLE `omoccurrences`
   ADD COLUMN `eventID` VARCHAR(45) NULL AFTER `fieldnumber`;
 
 
+
+
+
+
 # Add one to many relationship between collections and institutions
 # Add one to many relationship between collection to agent
 
@@ -27,3 +31,50 @@ ALTER TABLE `omoccurrences`
 
 #Collection GUID issue
 
+
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE `omoccurpoints`;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+INSERT INTO omoccurpoints (occid,point)
+SELECT occid,Point(decimalLatitude, decimalLongitude) FROM omoccurrences WHERE decimalLatitude IS NOT NULL AND decimalLongitude IS NOT NULL;
+
+DELIMITER //
+DROP TRIGGER IF EXISTS `omoccurpoints_insert`//
+CREATE TRIGGER `omoccurpoints_insert` AFTER INSERT ON `omoccurrences`
+FOR EACH ROW BEGIN
+  IF NEW.`decimalLatitude` IS NOT NULL AND NEW.`decimalLongitude` IS NOT NULL THEN
+	  INSERT INTO omoccurpoints (
+		`occid`,
+		`point`
+	  ) VALUES (
+		NEW.`occid`,
+		Point(NEW.`decimalLatitude`, NEW.`decimalLongitude`)
+	  );
+  END IF;
+END
+//
+
+DROP TRIGGER IF EXISTS `omoccurpoints_update`//
+CREATE TRIGGER `omoccurpoints_update` AFTER UPDATE ON `omoccurrences`
+FOR EACH ROW BEGIN
+  IF NEW.`decimalLatitude` IS NOT NULL AND NEW.`decimalLongitude` IS NOT NULL THEN
+	  UPDATE omoccurpoints SET
+		`point` = Point(NEW.`decimalLatitude`, NEW.`decimalLongitude`)
+	  WHERE `occid` = NEW.`occid`;
+  END IF;
+END
+//
+
+DROP TRIGGER IF EXISTS `omoccurpoints_delete`//
+CREATE TRIGGER `omoccurpoints_delete` BEFORE DELETE ON `omoccurrences`
+FOR EACH ROW BEGIN
+  DELETE FROM omoccurpoints WHERE `occid` = OLD.`occid`;
+END
+//
+
+DELIMITER ;
