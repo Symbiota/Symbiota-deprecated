6e3817c66308c9e16f83c6988a492de8b94f579a
bdbrandt <bdbrandt@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-04-02 21:20:28 +0000

    Addition of css version variable to all root directory files and conversion of main template files to liquid theme.

--- index_template.php
+++ index_template.php
@@ -6,8 +6,8 @@ header("Content-Type: text/html; charset=".$charset);
 <html>
 <head>
 	<title><?php echo $defaultTitle?> Home</title>
-	<link href="css/base.css" type="text/css" rel="stylesheet" />
-	<link href="css/main.css" type="text/css" rel="stylesheet" />
+	<link href="css/base.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+	<link href="css/main.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
 	<meta name='keywords' content='' />
 	<script type="text/javascript">
 		<?php include_once('config/googleanalytics.php'); ?>
@@ -15,14 +15,13 @@ header("Content-Type: text/html; charset=".$charset);
 </head>
 <body>
 	<?php
-	$displayLeftMenu = "true";
 	include($serverRoot."/header.php");
 	?> 
         <!-- This is inner text! -->
         <div  id="innertext">
             <h1></h1>
 
-            <div style="margin:20px;">
+            <div style="padding: 0px 10px;">
             	Description and introduction of project
             </div>
         </div>
--- sitemap.php
+++ sitemap.php
@@ -10,8 +10,8 @@ $smManager = new SiteMapManager();
 <html>
 <head>
 	<title><?php echo $defaultTitle; ?> Site Map</title>
-	<link href="css/base.css" type="text/css" rel="stylesheet" />
-	<link href="css/main.css" type="text/css" rel="stylesheet" />
+	<link href="css/base.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+	<link href="css/main.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
 	<script type="text/javascript">
 		function submitTaxaNoImgForm(f){
 			if(f.clid.value != ""){
