89985fd1dac77ed0a59ac15faba7e1ee9859dd4e
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-06-11 21:34:32 +0000

    - Bug fix which inhibited rare species administrators and super admin from being able to create a checklist of the state rare species protection type

--- trunk/js/symb/checklists.checklistadmin.js
+++ trunk/js/symb/checklists.checklistadmin.js
@@ -28,40 +28,44 @@ function openPointAid(latDef,lngDef) {
     if(pointWindow.opener == null) pointWindow.opener = self;
 }
 
-function validateMetadataForm(f){ 
-	if(f.ecllatcentroid.value == "" && f.ecllongcentroid.value == ""){
-		return true;
-	}
-	if(f.ecllatcentroid.value == ""){
-		alert("If longitude has a value, latitude must also have a value");
-		return false;
+function validateMetadataForm(f){
+	if(f.ecllatcentroid.value != ""){
+		if(f.ecllongcentroid.value == ""){
+			alert("If longitude has a value, latitude must also have a value");
+			return false;
+		}
+		if(!isNumeric(f.ecllatcentroid.value)){
+			alert("Latitude must be strictly numeric (decimal format: e.g. 34.2343)");
+			return false;
+		}
+		if(Math.abs(f.ecllatcentroid.value) > 90){
+			alert("Latitude values can not be greater than 90 or less than -90.");
+			return false;
+		} 
 	} 
-	if(f.ecllongcentroid.value == ""){
-		alert("If latitude has a value, longitude must also have a value");
-		return false;
-	} 
-	if(!isNumeric(f.ecllatcentroid.value)){
-		alert("Latitude must be strictly numeric (decimal format: e.g. 34.2343)");
-		return false;
-	}
-	if(Math.abs(f.ecllatcentroid.value) > 90){
-		alert("Latitude values can not be greater than 90 or less than -90.");
-		return false;
-	} 
-	if(!isNumeric(f.ecllongcentroid.value)){
-		alert("Longitude must be strictly numeric (decimal format: e.g. -112.2343)");
-		return false;
-	}
-	if(Math.abs(f.ecllongcentroid.value) > 180){
-		alert("Longitude values can not be greater than 180 or less than -180.");
-		return false;
-	}
-	if(f.ecllongcentroid.value > 1){
-		alert("Is this checklist in the western hemisphere?\nIf so, decimal longitude should be a negative value (e.g. -112.2343)");
+	if(f.ecllongcentroid.value != ""){
+		if(f.ecllatcentroid.value == ""){
+			alert("If latitude has a value, longitude must also have a value");
+			return false;
+		}
+		if(!isNumeric(f.ecllongcentroid.value)){
+			alert("Longitude must be strictly numeric (decimal format: e.g. -112.2343)");
+			return false;
+		}
+		if(Math.abs(f.ecllongcentroid.value) > 180){
+			alert("Longitude values can not be greater than 180 or less than -180.");
+			return false;
+		}
 	} 
 	if(!isNumeric(f.eclpointradiusmeters.value)){
 		alert("Point radius must be a numeric value only");
 		return false;
+	}
+	if(f.ecltype){ 
+		if(f.ecltype.value == "rarespp" && f.ecllocality.value == ""){
+			alert("Rare species checklists must have a state value entered into the locality field");
+			return false;
+		}
 	}
 	return true;
 }
