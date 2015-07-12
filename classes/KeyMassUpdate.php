787915e25be03047296ca59dc784467f14387ab3
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-07-12 15:54:44 +0000

    - Modifications to the identification key management tool that allows one to code characters within a matrix display. mainly fixed taxonomic hierarchy display errors.

--- classes/KeyMassUpdate.php
+++ classes/KeyMassUpdate.php
@@ -4,6 +4,11 @@ include_once($serverRoot.'/classes/KeyManager.php');
 class KeyMassUpdate extends KeyManager{
 	
 	private $cid;
+	private $taxaArr = array();
+	private $stateArr = array();
+	private $descrArr = array();
+	private $headerStr;
+	private $cnt = 0;
 	
 	public function __construct(){
 		parent::__construct();
@@ -35,28 +40,24 @@ class KeyMassUpdate extends KeyManager{
 		return $headingArray;
 	}
 	
-	public function getStates(){
-		$stateArr = Array();
-		$sql = 'SELECT kmcs.CharStateName, kmcs.CS FROM kmcs '.
-			'WHERE (kmcs.CID = '.$this->cid.') ';
+	private function setStates(){
+		$sql = 'SELECT charstatename, cs FROM kmcs WHERE (cid = '.$this->cid.') ';
 		$rs = $this->conn->query($sql);
 		while($row = $rs->fetch_object()){
-			$stateArr[$row->CS] = $row->CharStateName;
+			$this->stateArr[$row->cs] = $row->charstatename;
 		}
 		$rs->free();
-		ksort($stateArr);
-		return $stateArr;
+		ksort($this->stateArr);
 	}
 
-	public function getTaxaList($clidFilter, $tidFilter, $generaOnly = false){
-		//Get all Taxa found in checklist 
-		$retArr = Array();
+	public function echoTaxaList($clidFilter, $tidFilter, $generaOnly = false){
 		$tidArr = Array();
+		
 		$sqlBase = '';
 		$sqlWhere = '';
 		if($clidFilter){
-			$sqlBase .= 'INNER JOIN fmchklsttaxalink ctl ON ts.tid = ctl.tid ';
-			$sqlWhere .= 'AND (ctl.CLID = '.$clidFilter.') ' ;
+			$sqlBase .= 'INNER JOIN fmchklsttaxalink c ON ts.tid = c.tid ';
+			$sqlWhere .= 'AND (c.clid = '.$clidFilter.') ' ;
 		}
 		if($tidFilter){
 			$sqlBase .= 'INNER JOIN taxaenumtree e ON ts.tid = e.tid ';
@@ -64,52 +65,111 @@ class KeyMassUpdate extends KeyManager{
 		}
 		//Get accepted taxa
 		if(!$generaOnly){
-			$sql = 'SELECT DISTINCT t.tid, ts.family, t.sciname, t.rankid '.
-				'FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tidaccepted '.$sqlBase.
-				'WHERE (ts.taxauthid = '.$this->taxAuthId.') '.$sqlWhere;
+			$sql = 'SELECT DISTINCT t.tid, t.sciname, ts2.parenttid '.
+				'FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tidaccepted '.
+				'INNER JOIN taxstatus ts2 ON t.tid = ts2.tid '.$sqlBase.
+				'WHERE (ts.taxauthid = '.$this->taxAuthId.') AND (ts2.taxauthid = '.$this->taxAuthId.') AND (t.rankid = 220) '.$sqlWhere;
 			//echo $sql; exit;
 			$rs = $this->conn->query($sql);
 			while($r = $rs->fetch_object()){
-				if($r->rankid == 220){
-					$tidArr[] = $r->tid;
-					$retArr[$r->family][$r->sciname] = $r->tid;
-				}
+				$this->taxaArr[$r->parenttid][$r->tid] = $r->sciname;
+				$tidArr[] = $r->tid;
 			}
 			$rs->free();
 		}
 		
 		//Get parents
-		$sql = 'SELECT DISTINCT t.tid, ts.family, t.sciname, t.rankid '.
-			'FROM taxa t INNER JOIN taxaenumtree et ON t.tid = et.parenttid '.
-			'INNER JOIN taxstatus ts ON et.tid = ts.tidaccepted '.$sqlBase.
-			'WHERE (ts.taxauthid = '.$this->taxAuthId.') AND (t.rankid >= 140) AND (t.rankid <= 220) '.$sqlWhere;
-		//echo $sql; exit;
-		$rs = $this->conn->query($sql);
-		while($r = $rs->fetch_object()){
-			if($r->rankid < 220 || !$generaOnly){
-				$tidArr[] = $r->tid;
-				$family = $r->family;
-				if($r->rankid == 140 && !$family) $family = $r->sciname;
-				$retArr[$family][$r->sciname] = $r->tid;
+		$famArr = array();
+		$sql2 = 'SELECT DISTINCT t.tid, t.sciname, ts2.parenttid, t.rankid '.
+			'FROM taxa t INNER JOIN taxaenumtree e2 ON t.tid = e2.parenttid '.
+			'INNER JOIN taxstatus ts2 ON t.tid = ts2.tid '.
+			'INNER JOIN taxstatus ts ON e2.tid = ts.tidaccepted '.$sqlBase.
+			'WHERE (ts.taxauthid = '.$this->taxAuthId.') AND (ts2.taxauthid = '.$this->taxAuthId.') AND (e2.taxauthid = '.$this->taxAuthId.') '.
+			'AND (t.rankid <= 220) AND (t.rankid >= 140) '.$sqlWhere;
+		//echo $sql2; exit;
+		$rs2 = $this->conn->query($sql2);
+		while($r2 = $rs2->fetch_object()){
+			if($r2->rankid == 140){
+				$famArr[$r2->tid] = $r2->sciname;
 			}
+			elseif(!$generaOnly || $r2->rankid < 220){
+				$this->taxaArr[$r2->parenttid][$r2->tid] = $r2->sciname;
+			}
+			$tidArr[] = $r2->tid;
 		}
-		$rs->free();
+		$rs2->free();
 
 		//Get descriptions
 		if($tidArr){
-			$sql = 'SELECT tid, cid, cs, inherited FROM kmdescr '.
+			$sql3 = 'SELECT tid, cid, cs, inherited FROM kmdescr '.
 				'WHERE (cid='.$this->cid.') AND (tid IN('.implode(',',$tidArr).'))';
-			$rs = $this->conn->query($sql);
-			while($r = $rs->fetch_object()){
-				$i = 0;
-				if($r->inherited) $i = 1;
-				$retArr['cs'][$r->tid][$r->cs] = $i;
+			$rs3 = $this->conn->query($sql3);
+			while($r3 = $rs3->fetch_object()){
+				$this->descrArr[$r3->tid][$r3->cs] = ($r3->inherited?1:0);
 			}
-			$rs->free();
+			$rs3->free();
 		}
-		return $retArr;
+		
+		//Create and output header 
+		$this->setStates();
+		$this->headerStr = '<tr><th/>';
+ 		foreach($this->stateArr as $cs => $csName){
+ 			$this->headerStr .= '<th>'.str_replace(" ","<br/>",$csName).'</th>';
+ 		}
+ 		$this->headerStr .= '</tr>'."\n";
+ 		$this->headerStr .= '<tr><td align="right" colspan="'.(count($this->stateArr)+1).'"><input type="submit" name="action" value="Save Changes" onclick="submitAttrs()" /></td></tr>';
+ 		echo $this->headerStr;
+		
+		//Output data, including header every 12 times 
+		$cnt = 0;
+		foreach($famArr as $famTid => $family){
+			$this->echoTaxaRow($famTid,$family);
+			$this->processTaxa($famTid);
+		}
+		echo $this->headerStr;
+	}
+	
+	private function processTaxa($tid,$indent=0){
+		if(isset($this->taxaArr[$tid])){
+			$indent++;
+			$childArr = $this->taxaArr[$tid];
+			asort($childArr);
+			foreach($childArr as $childTid => $childSciname){
+				$this->echoTaxaRow($childTid,$childSciname,$indent);
+				$this->processTaxa($childTid,$indent);
+			}
+		}
 	}
 
+	private function echoTaxaRow($tid,$sciname,$indent = 0){
+		echo '<tr><td>';
+		echo '<span style="margin-left:'.($indent*10).'px"><b>'.($indent?'<i>':'').$sciname.($indent?'</i>':'').'</b></span>';
+		echo '<a href="editor.php?tid='.$tid.'" target="_blank"><img src="../../images/edit.png" /></a>';
+		echo '</td>';
+		foreach($this->stateArr as $cs => $csName){
+			$isSelected = false;
+			$isInherited = false;
+			if(isset($this->descrArr[$tid][$cs])){
+				$isSelected = true;
+				if($this->descrArr[$tid][$cs]) $isInherited = true;
+			}
+			if($isSelected && !$isInherited){
+				//State is true and not inherited for this taxon
+				$jsStr = "removeAttr('".$tid."-".$cs."');";
+			}
+			else{
+				//State is false for this taxon or it is inherited
+				$jsStr = "addAttr('".$tid."-".$cs."');";
+			}
+			echo '<td align="center" style="width:15px;white-space:nowrap;">';
+			echo '<input type="checkbox" name="csDisplay" onclick="'.$jsStr.'" '.($isSelected && !$isInherited?'CHECKED':'').' title="'.$csName.'"/>'.($isInherited?'(I)':'');
+			echo "</td>\n";
+		}
+		echo '</tr>';
+		$this->cnt++;
+		if($this->cnt%40 == 0) echo $this->headerStr;
+	}
+	
 	public function processAttributes($rAttrs,$aAttrs){
 		$removeArr = $this->processAttrArr($rAttrs);
 		$addArr = $this->processAttrArr($aAttrs);
@@ -178,7 +238,7 @@ class KeyMassUpdate extends KeyManager{
 		$retArr = Array();
 		$sql = 'SELECT t.tid, t.sciname '. 
 			'FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tid '.
-			'WHERE (t.rankid >= 140) AND (t.rankid <= 180) AND (ts.tid = ts.tidaccepted) AND (ts.taxauthid = 1) '.
+			'WHERE (t.rankid >= 140) AND (t.rankid < 180) AND (ts.tid = ts.tidaccepted) AND (ts.taxauthid = 1) '.
 			'ORDER BY t.rankid, t.sciname ';
 		//echo $sql;
 		$rs = $this->conn->query($sql);
