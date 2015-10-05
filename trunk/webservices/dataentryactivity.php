7c181ddbaf529cc64c5c885ca4f870b9a8a2305d
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-10-05 02:44:36 +0000

    - Added an RSS feed that shows latest data entry activity, as requested by WeDigBio crowdsourcing data entry marathon

---
+++ trunk/webservices/dataentryactivity.php
@@ -0,0 +1,17 @@
+<?php
+include_once('../config/symbini.php');
+include_once($serverRoot.'/classes/OccurrenceDownload.php');
+
+$format = isset($_REQUEST['format'])&&$_REQUEST['format']?$_REQUEST['format']:'rss';
+$days = isset($_REQUEST['days'])?$_REQUEST['days']:5;
+
+$activityManager = new OccurrenceDownload();
+
+header('Content-Description: '.$GLOBALS['DEFAULT_TITLE'].' Data Entry Activity');
+header('Content-Type: '.($format=='json'?'application/json':'text/xml'));
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+echo $activityManager->getDataEntryActivity($format,$days);
+?>
