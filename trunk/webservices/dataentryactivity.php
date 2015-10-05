873a350bebeb8c07492a1373be5d7462eefe22aa
egbot <egbot@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-10-05 19:35:11 +0000

    - Minor adjustments and debugging of the occurrence editing activity RSS feed

--- trunk/webservices/dataentryactivity.php
+++ trunk/webservices/dataentryactivity.php
@@ -3,7 +3,8 @@ include_once('../config/symbini.php');
 include_once($serverRoot.'/classes/OccurrenceDownload.php');
 
 $format = isset($_REQUEST['format'])&&$_REQUEST['format']?$_REQUEST['format']:'rss';
-$days = isset($_REQUEST['days'])?$_REQUEST['days']:5;
+$days = isset($_REQUEST['days'])?$_REQUEST['days']:0;
+$limit = isset($_REQUEST['limit'])?$_REQUEST['limit']:0;
 
 $activityManager = new OccurrenceDownload();
 
@@ -13,5 +14,5 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Cache-Control: no-cache");
 header("Pragma: no-cache");
 
-echo $activityManager->getDataEntryActivity($format,$days);
+echo $activityManager->getDataEntryActivity($format,$days,$limit);
 ?>
