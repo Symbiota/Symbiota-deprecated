574d22512840515cc356bf6b3b3a8d82831bbf0b
bdbrandt <bdbrandt@6bc6c840-c11a-4335-b936-cfadb8367715>
2015-10-30 17:30:16 +0000

    Minor fix to collection profile, and collection statistics pages which corrects for potential error occurring if dynamicProperties field is not set properly.

--- collections/misc/collstatscsv.php
+++ collections/misc/collstatscsv.php
@@ -17,29 +17,33 @@ $header = array('Names','SpecimenCount','GeorefCount','IDCount','IDGeorefCount',
 if($action == 'Download Family Dist'){
 	$fileName = 'stats_family.csv';
 	$famArr = json_decode($famArrJson,true);
-	foreach($famArr as $name => $data){
-		$specCnt = $data['SpecimensPerFamily'];
-		$geoRefCnt = $data['GeorefSpecimensPerFamily'];
-		$IDCnt = $data['IDSpecimensPerFamily'];
-		$IDGeoRefCnt = $data['IDGeorefSpecimensPerFamily'];
-		$geoRefPerc = ($data['GeorefSpecimensPerFamily']?round(100*($data['GeorefSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
-		$IDPerc = ($data['IDSpecimensPerFamily']?round(100*($data['IDSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
-		$IDgeoRefPerc = ($data['IDGeorefSpecimensPerFamily']?round(100*($data['IDGeorefSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
-		array_push($outputArr,array($name,$specCnt,$geoRefCnt,$IDCnt,$IDGeoRefCnt,$geoRefPerc,$IDPerc,$IDgeoRefPerc));
+	if(is_array($famArr)){
+		foreach($famArr as $name => $data){
+			$specCnt = $data['SpecimensPerFamily'];
+			$geoRefCnt = $data['GeorefSpecimensPerFamily'];
+			$IDCnt = $data['IDSpecimensPerFamily'];
+			$IDGeoRefCnt = $data['IDGeorefSpecimensPerFamily'];
+			$geoRefPerc = ($data['GeorefSpecimensPerFamily']?round(100*($data['GeorefSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
+			$IDPerc = ($data['IDSpecimensPerFamily']?round(100*($data['IDSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
+			$IDgeoRefPerc = ($data['IDGeorefSpecimensPerFamily']?round(100*($data['IDGeorefSpecimensPerFamily']/$data['SpecimensPerFamily'])):0);
+			array_push($outputArr,array($name,$specCnt,$geoRefCnt,$IDCnt,$IDGeoRefCnt,$geoRefPerc,$IDPerc,$IDgeoRefPerc));
+		}
 	}
 }
 if($action == 'Download Geo Dist'){
 	$fileName = 'stats_country.csv';
 	$geoArr = json_decode($geoArrJson,true);
-	foreach($geoArr as $name => $data){
-		$specCnt = $data['CountryCount'];
-		$geoRefCnt = $data['GeorefSpecimensPerCountry'];
-		$IDCnt = $data['IDSpecimensPerCountry'];
-		$IDGeoRefCnt = $data['IDGeorefSpecimensPerCountry'];
-		$geoRefPerc = ($data['GeorefSpecimensPerCountry']?round(100*($data['GeorefSpecimensPerCountry']/$data['CountryCount'])):0);
-		$IDPerc = ($data['IDSpecimensPerCountry']?round(100*($data['IDSpecimensPerCountry']/$data['CountryCount'])):0);
-		$IDgeoRefPerc = ($data['IDGeorefSpecimensPerCountry']?round(100*($data['IDGeorefSpecimensPerCountry']/$data['CountryCount'])):0);
-		array_push($outputArr,array($name,$specCnt,$geoRefCnt,$IDCnt,$IDGeoRefCnt,$geoRefPerc,$IDPerc,$IDgeoRefPerc));
+	if(is_array($geoArr)){
+		foreach($geoArr as $name => $data){
+			$specCnt = $data['CountryCount'];
+			$geoRefCnt = $data['GeorefSpecimensPerCountry'];
+			$IDCnt = $data['IDSpecimensPerCountry'];
+			$IDGeoRefCnt = $data['IDGeorefSpecimensPerCountry'];
+			$geoRefPerc = ($data['GeorefSpecimensPerCountry']?round(100*($data['GeorefSpecimensPerCountry']/$data['CountryCount'])):0);
+			$IDPerc = ($data['IDSpecimensPerCountry']?round(100*($data['IDSpecimensPerCountry']/$data['CountryCount'])):0);
+			$IDgeoRefPerc = ($data['IDGeorefSpecimensPerCountry']?round(100*($data['IDGeorefSpecimensPerCountry']/$data['CountryCount'])):0);
+			array_push($outputArr,array($name,$specCnt,$geoRefCnt,$IDCnt,$IDGeoRefCnt,$geoRefPerc,$IDPerc,$IDgeoRefPerc));
+		}
 	}
 }
 if($action == 'Download CSV'){
