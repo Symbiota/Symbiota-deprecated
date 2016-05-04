6553c71980a6fb2b765eedb456d62b4e23a14c29
Edward Gilbert <egbiodiversity@gmail.com>
2016-05-04 00:23:59 -0700

    Minor updates
    - Minor updates to language config file
    - Removal of old AJAX syntax

--- content/lang/collections/list.en.php
+++ content/lang/collections/list.en.php
@@ -9,6 +9,7 @@ include_once('sharedterms.en.php');
 
 $LANG['PAGE_TITLE'] = 'Collections Search Results';
 $LANG['NAV_SPECIMEN_LIST'] = 'Specimen Records';
+$LANG['DOWNLOAD_SPECIMEN_DATA'] = 'Download Specimen Data';
 $LANG['DATASET'] = 'Dataset';
 $LANG['TAXA'] = 'Taxa';
 $LANG['SEARCH_CRITERIA'] = 'Search Criteria';
@@ -24,8 +25,7 @@ $LANG['PERHAPS_LOOKING_FOR'] = 'Perhaps you were looking for:';
 $LANG['MAP_DOWNLOAD'] = 'Download Coordinate Data';
 $LANG['GOOGLE_MAP_HEADER'] = 'Google Map';
 $LANG['GOOGLE_MAP_DISPLAY'] = 'Display coordinates in Google Map';
-$LANG['GOOGLE_MAP_DESCRIPTION'] = 'Google Maps is a web mapping service provided by Google that features a map that users can pan (by dragging the mouse) and zoom (by using the mouse wheel). Collection points are 
-	displayed as colored markers that when clicked on, displays the full information for that collection. When multiple species are queried (separated by semi-colons), different colored markers denote each individual species.';
+$LANG['GOOGLE_MAP_DESCRIPTION'] = 'Google Maps is a web mapping service provided by Google that features a map that users can pan (by dragging the mouse) and zoom (by using the mouse wheel). Collection points are displayed as colored markers that when clicked on, displays the full information for that collection. When multiple species are queried (separated by semi-colons), different colored markers denote each individual species.';
 $LANG['GOOGLE_EARTH_HEADER'] = 'Google Earth (KML)';
 $LANG['GOOGLE_EARTH_DESCRIPTION'] = 'This creates an KML file that can be opened in the Google Earth mapping application. Note that you must have <a href="http://earth.google.com/" target="_blank"> Google Earth</a> installed on your computer to make use of this option.';
 $LANG['GOOGLE_EARTH_EXTRA'] = 'Add Extra Fields';
