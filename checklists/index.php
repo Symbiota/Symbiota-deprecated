b67e8b27ce436e4d8d64d0f5a48ef0b4dc7e30fc
Edward Gilbert <egbiodiversity@gmail.com>
2016-06-23 08:19:33 -0800

    Minor Adjustments to checklist display on checklist index page
    - Expanded list of inventory checklists beyond those linked to only
    project
    - Minor adjustment to layout of project list

--- checklists/index.php
+++ checklists/index.php
@@ -3,12 +3,10 @@ include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ChecklistManager.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
-$projValue = array_key_exists("proj",$_REQUEST)?$_REQUEST["proj"]:""; 
-if(!$projValue && isset($DEFAULT_PROJ_ID)) $projValue = $DEFAULT_PROJ_ID;
+$pid = array_key_exists("pid",$_REQUEST)?$_REQUEST["pid"]:0; 
 
 $clManager = new ChecklistManager();
-$clManager->setProj($projValue);
-$pid = $clManager->getPid();
+$clManager->setProj($pid);
 ?>
 <html>
 <head>
@@ -25,13 +23,11 @@ $pid = $clManager->getPid();
 	<?php
 	$displayLeftMenu = (isset($checklists_indexMenu)?$checklists_indexMenu:"true");
 	include($SERVER_ROOT."/header.php");
-	if(isset($checklists_indexCrumbs)){
-		echo "<div class='navpath'>";
-		echo "<a href='../index.php'>Home</a> &gt; ";
-		echo $checklists_indexCrumbs;
-		echo " <b>Species Checklists</b>";
-		echo "</div>";
-	}
+	echo "<div class='navpath'>";
+	echo "<a href='../index.php'>Home</a> &gt;&gt; ";
+	if(isset($checklists_indexCrumbs) && $checklists_indexCrumbs) echo $checklists_indexCrumbs.' &gt;&gt;';
+	echo " <b>Species Checklists</b>";
+	echo "</div>";
 	?>
 	<!-- This is inner text! -->
 	<div id="innertext">
@@ -40,26 +36,29 @@ $pid = $clManager->getPid();
 			<?php 
             $researchArr = $clManager->getChecklists();
 			if($researchArr){
-				$pName = $researchArr['name'];
-				$clArr = $researchArr['clid'];
-				?>
-				<div style='margin:3px 0px 0px 15px;'>
-					<h3><?php echo $pName; ?> 
-						<a href="<?php echo "clgmap.php?proj=".$pid; ?>" title='Show checklists on map'>
-							<img src='../images/world.png' style='width:10px;border:0' />
-						</a>
-					</h3>
-					<div>
-						<ul>
-							<?php 
-							foreach($clArr as $clid => $clName){
-								echo "<li><a href='checklist.php?cl=".$clid."'>".$clName."</a></li>\n";
-							}
-							?>
-						</ul>
+				foreach($researchArr as $pid => $projArr){
+					?>
+					<div style='margin:3px 0px 0px 15px;'>
+						<h3><?php echo $projArr['name']; ?> 
+							<a href="<?php echo "clgmap.php?proj=".$pid; ?>" title='Show checklists on map'>
+								<img src='../images/world.png' style='width:10px;border:0' />
+							</a>
+						</h3>
+						<div>
+							<ul>
+								<?php 
+								foreach($projArr['clid'] as $clid => $clName){
+									echo "<li><a href='checklist.php?cl=".$clid."'>".$clName."</a></li>\n";
+								}
+								?>
+							</ul>
+						</div>
 					</div>
-				</div>
-				<?php 
+					<?php
+				}
+			}
+			else{
+				echo '<div><b>No Checklists returned</b></div>';
 			}
 			?>
 		</div>
