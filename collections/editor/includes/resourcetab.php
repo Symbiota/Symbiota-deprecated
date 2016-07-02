0f8cbe00bf492df0d7e96b269ac71d7e962c4770
Edward Gilbert <egbiodiversity@gmail.com>
2016-07-01 18:56:37 -0800

    Modifications to duplicate linking with Occurrence Editor
    - Finished enabling duplicate search and linking tool within the
    occurrence editor

--- collections/editor/includes/resourcetab.php
+++ collections/editor/includes/resourcetab.php
@@ -21,48 +21,10 @@ $lastName = $dupManager->parseLastName($occArr['recordedby']);
 ?>
 <script type="text/javascript">
 
-	$(function() {
-		var dialog;
-
-		dialog = $( "#dialog-dupelist" ).dialog({
-			autoOpen: false,
-			height: 300,
-			width: 350,
-			modal: true,
-			close: function() {
-			}
-		});
-	});
-
-	function searchForDuplicates(){
-		if(!$("#dup-catnum").val() && !$("#dup-occid").val() && (!$("#dup-lastname").val() && (!$("#dup-collnum").val() || !$("#dup-colldate").val()))){
-			alert("Catalog number or collector field must contain values that will be used in the search");
-			return false;
-		}
-		
-		$.ajax({
-			type: "POST",
-			url: "rpc/dupequery.php",
-			dataType: "json",
-			data: { dupid: dupid, occid: occid }
-		}).done(function( retStr ) {
-			if(retStr == "1"){
-				$("#dupediv-"+occid).hide();
-				dialog.dialog( "open" );
-			}
-			else{
-				alert("ERROR deleting duplicate: "+retStr);
-			}
-		});
-	}
-
-	function addToDupList() {
-		$( "#users tbody" ).append( "<tr>" +
-			"<td>" + name.val() + "</td>" +
-			"<td>" + email.val() + "</td>" +
-			"<td>" + password.val() + "</td>" +
-			"</tr>" );
-			dialog.dialog( "close" );
+	function openDupeWindow(){
+		$url = "rpc/dupelist.php?curoccid=<?php echo $occid.'&lastname='.$occArr['recordedby'].'&collnum='.$occArr['recordnumber'].'&colldate='.$occArr['eventdate']; ?>";
+		dupeWindow=open($url,"dupelist","resizable=1,scrollbars=1,toolbar=1,width=900,height=600,left=20,top=20");
+		if (dupeWindow.opener == null) dupeWindow.opener = self;
 	}
 
 	function deleteDuplicateLink(dupid, occid){
@@ -113,93 +75,69 @@ $lastName = $dupManager->parseLastName($occArr['recordedby']);
 	}
 </script>
 
-<div id="duplicatediv"  style="width:795px;">
+<div id="duplicatediv" style="width:795px;">
 	<fieldset>
 		<legend><b>Duplicate Specimens</b></legend>
-		<div style="float:right;">
-			<a href="#" onclick="toggle('dupadddiv');return false;" title="Link a duplicate specimen" ><img src="../../images/add.png" /></a>
+		<div style="float:right;margin-right:15px;">
+			<button onclick="openDupeWindow();return false;">Search for Records to Link</button>
 		</div>
-		<div id="dialog-dupelist">
-			
-			
-		</div>
-		<div id="dupadddiv" style="display:none;">
-			<fieldset>
-				<legend><b>Link New Specimen</b></legend>
-				<form name="adddupform" method="post" action="occurrenceeditor.php">
-					<div style="margin:3px;">
-						<label for="dup-lastname">Last Name</label>
-						<input type="text" name="dup-lastname" id="dup-lastname" value="<?php echo $lastName; ?>" class="text ui-widget-content ui-corner-all" />
-					</div>
-					<div style="margin:3px;">
-						<label for="dup-collnum">Number</label>
-						<input type="text" name="dup-collnum" id="dup-collnum" value="<?php echo $occArr['recordnumber'] ?>" class="text ui-widget-content ui-corner-all" />
-					</div>
-					<div style="margin:3px;">
-						<label for="dup-colldate">Date</label>
-						<input type="text" name="dup-colldate" id="dup-colldate" value="<?php echo $occArr['eventdate'] ?>" class="text ui-widget-content ui-corner-all" />
-					</div>
-					<div style="margin:3px;">
-						<label for="dup-catnum">Catalog Number</label>
-						<input type="text" name="dup-catnum" id="dup-catnum" value="" class="text ui-widget-content ui-corner-all" />
-					</div>
-					<div style="margin:3px;">
-						<label for="dup-occid">occid(s)</label>
-						<input type="text" name="dup-occid" id="dup-occid" value="" class="text ui-widget-content ui-corner-all" />
-						<button id="dup-search" style="margin-left:20px;" onclick="searchForDuplicates()">Search for Duplicates</button>
- 					</div>
-				</form>
-			</fieldset>
-		</div>
-		<div>
+		<div style="clear:both;">
+			<form id="dupeRefreshForm" name="dupeRefreshForm" method="post" target="occurrenceeditor.php">
+				<input name="tabtarget" type="hidden" value="3" />
+				<input name="occid" type="hidden" value="<?php echo $occid; ?>" />
+			</form>
 			<?php
 			if($dupClusterArr){ 
 				foreach($dupClusterArr as $dupid => $dupArr){
 					echo '<div id="dupediv-'.$occid.'">';
 					echo '<div style="padding:15px;"><b>Cluster Title:</b> '.$dupArr['title'];
-					echo '<div style="float:right" title="Unlink this occurrences from duplicate cluster but maintain other specimens as a valid duplicate cluster"><button name="unlinkthisdupebutton" onclick="deleteDuplicateLink('.$dupid.','.$occid.')">Remove this Occurrence from Cluster</button></div>';
+					echo '<div style="float:right" title="Unlink this occurrences from duplicate cluster but maintain other specimens as a valid duplicate cluster">';
+					echo '<button name="unlinkthisdupebutton" onclick="deleteDuplicateLink('.$dupid.','.$occid.')">Remove this Occurrence from Cluster</button>';
+					echo '</div>';
 					$note = trim($dupArr['description'].'; '.$dupArr['notes'],' ;');
 					if($note) echo ' - '.$notes;
 					echo '</div>';
 					echo '<div style="20px 0px"><hr/><hr/></div>';
 					$innerDupArr = $dupArr['o'];
 					foreach($innerDupArr as $dupeOccid => $dArr){
-						?>
-						<div id="dupediv-<?php echo $dupeOccid; ?>" style="clear:both;margin:15px;">
-							<div style="font-weight:bold;font-size:120%;">
-								<?php echo $dArr['collname'].' ('.$dArr['instcode'].($dArr['collcode']?':'.$dArr['collcode']:'').')'; ?>
-							</div>
-							<div style="float:right;">
-								<button name="unlinkdupebut" onclick="deleteDuplicateLink(<?php echo $dupid.','.$dupeOccid; ?>)">Unlink</button>
-							</div>
-							<?php 
-							echo '<div style="float:left;margin:5px 15px">';
-							if($dArr['recordedby']) echo '<div>'.$dArr['recordedby'].' '.$dArr['recordnumber'].'<span style="margin-left:40px;">'.$dArr['eventdate'].'</span></div>';
-							if($dArr['catnum']) echo '<div><b>Catalog Number:</b> '.$dArr['catnum'].'</div>';
-							if($dArr['occurrenceid']) echo '<div><b>GUID:</b> '.$dArr['occurrenceid'].'</div>';
-							if($dArr['sciname']) echo '<div><b>Latest Identification:</b> '.$dArr['sciname'].'</div>';
-							if($dArr['identifiedby']) echo '<div><b>Identified by:</b> '.$dArr['identifiedby'].'<span stlye="margin-left:30px;">'.$dArr['dateidentified'].'</span></div>';
-							if($dArr['notes']) echo '<div>'.$dArr['notes'].'</div>';
-							echo '<div><a href="#" onclick="openIndividual('.$dupeOccid.')">Show Full Details</a></div>';
-							echo '</div>';
-							if($dArr['url']){
-								$url = $dArr['url'];
-								$tnUrl = $dArr['tnurl'];
-								if(!$tnUrl) $tnUrl = $url;
-								if($IMAGE_DOMAIN){
-									if(substr($url,0,1) == '/') $url = $IMAGE_DOMAIN.$url;
-									if(substr($tnUrl,0,1) == '/') $tnUrl = $IMAGE_DOMAIN.$tnUrl;
-								}
-								echo '<div style="float:left;margin:10px;">';
-								echo '<a href="'.$url.'">';
-								echo '<img src="'.$tnUrl.'" style="width:100px;border:1px solid grey" />';
-								echo '</a>';
-								echo '</div>';
-							}
-							echo '<div style="margin:10px 0px;clear:both"><hr/></div>';
+						if($occid != $dupeOccid){
 							?>
-						</div>
-						<?php
+							<div id="dupediv-<?php echo $dupeOccid; ?>" style="clear:both;margin:15px;">
+								<div style="font-weight:bold;font-size:120%;">
+									<?php echo $dArr['collname'].' ('.$dArr['instcode'].($dArr['collcode']?':'.$dArr['collcode']:'').')'; ?>
+								</div>
+								<div style="float:right;">
+									<button name="unlinkdupebut" onclick="deleteDuplicateLink(<?php echo $dupid.','.$dupeOccid; ?>)">Unlink</button>
+								</div>
+								<?php 
+								echo '<div style="float:left;margin:5px 15px">';
+								if($dArr['recordedby']) echo '<div>'.$dArr['recordedby'].' '.$dArr['recordnumber'].'<span style="margin-left:40px;">'.$dArr['eventdate'].'</span></div>';
+								if($dArr['catnum']) echo '<div><b>Catalog Number:</b> '.$dArr['catnum'].'</div>';
+								if($dArr['occurrenceid']) echo '<div><b>GUID:</b> '.$dArr['occurrenceid'].'</div>';
+								if($dArr['sciname']) echo '<div><b>Latest Identification:</b> '.$dArr['sciname'].'</div>';
+								if($dArr['identifiedby']) echo '<div><b>Identified by:</b> '.$dArr['identifiedby'].'<span stlye="margin-left:30px;">'.$dArr['dateidentified'].'</span></div>';
+								if($dArr['notes']) echo '<div>'.$dArr['notes'].'</div>';
+								echo '<div><a href="#" onclick="openIndividual('.$dupeOccid.')">Show Full Details</a></div>';
+								echo '</div>';
+								if($dArr['url']){
+									$url = $dArr['url'];
+									$tnUrl = $dArr['tnurl'];
+									if(!$tnUrl) $tnUrl = $url;
+									if($IMAGE_DOMAIN){
+										if(substr($url,0,1) == '/') $url = $IMAGE_DOMAIN.$url;
+										if(substr($tnUrl,0,1) == '/') $tnUrl = $IMAGE_DOMAIN.$tnUrl;
+									}
+									echo '<div style="float:left;margin:10px;">';
+									echo '<a href="'.$url.'">';
+									echo '<img src="'.$tnUrl.'" style="width:100px;border:1px solid grey" />';
+									echo '</a>';
+									echo '</div>';
+								}
+								echo '<div style="margin:10px 0px;clear:both"><hr/></div>';
+								?>
+							</div>
+							<?php
+						}
 					}
 					echo '</div>';
 				}
@@ -216,13 +154,13 @@ $lastName = $dupManager->parseLastName($occArr['recordedby']);
 		</div>
 	</fieldset>
 </div>
-<div id="geneticdiv"  style="width:795px;">
+<div id="geneticdiv" style="margin-top:20px;">
 	<fieldset>
 		<legend><b>Genetic Resources</b></legend>
 		<div style="float:right;">
 			<a href="#" onclick="toggle('genadddiv');return false;" title="Add a new genetic resource" ><img src="../../images/add.png" /></a>
 		</div>
-		<div id="genadddiv" style="display:<?php echo ($genticArr?'block':'none'); ?>;">
+		<div id="genadddiv" style="display:<?php echo ($genticArr?'none':'block'); ?>;">
 			<fieldset>
 				<legend><b>Add New Resource</b></legend>
 				<form name="addgeneticform" method="post" action="occurrenceeditor.php">
@@ -319,7 +257,6 @@ $lastName = $dupManager->parseLastName($occArr['recordedby']);
 				</div>
 				<?php
 			}
-			if(!$genticArr) echo '<div style="font-weight:bold;font-size:120%;margin:15px 0px;">No Genetic Resources linked to this record</div>';
 			?>
 		</div>
 	</fieldset>
---
+++ collections/editor/rpc/dupelist.php
@@ -0,0 +1,123 @@
+<?php
+include_once('../../../config/symbini.php');
+include_once($SERVER_ROOT.'/classes/OccurrenceDuplicate.php');
+
+$lastName = array_key_exists('lastname',$_REQUEST)?trim($_REQUEST['lastname']):'';
+$collNum = array_key_exists('collnum',$_REQUEST)?trim($_REQUEST['collnum']):'';
+$collDate = array_key_exists('colldate',$_REQUEST)?trim($_REQUEST['colldate']):'';
+$catNum = array_key_exists('catnum',$_POST)?trim($_POST['catnum']):'';
+$queryOccid = array_key_exists('occid',$_POST)?$_POST['occid']:'';
+$currentOccid = array_key_exists('curoccid',$_REQUEST)?$_REQUEST['curoccid']:'';
+$dupeOccid = array_key_exists('dupeoccid',$_POST)?$_POST['dupeoccid']:'';
+$dupeTitle = array_key_exists('dupetitle',$_POST)?$_POST['dupetitle']:'';
+$action = array_key_exists('submitaction',$_REQUEST)?$_REQUEST['submitaction']:'';
+
+$dupeManager = new OccurrenceDuplicate();
+$dupArr = $dupeManager->getDupeList($lastName, $collNum, $collDate, $catNum, $queryOccid, $currentOccid);
+
+?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
+	<title><?php echo $defaultTitle; ?> Occurrence Editor</title>
+	<script>
+		<?php 
+		if($action == 'Link as Duplicate'){
+			$dupeManager->linkDuplicates($currentOccid,$dupeOccid,$dupeTitle);
+			echo 'window.opener.document.getElementById("dupeRefreshForm").submit();';
+			echo 'self.close();';
+		}
+		?>
+		
+		function validateDupeForm(f){
+
+
+			return true;
+		}
+
+		function openIndWindow(occid){
+			$url = "../../individual/index.php?occid="+occid;
+			indWindow=open($url,"indlist","resizable=1,scrollbars=1,toolbar=1,width=1000,height=800,left=100,top=100");
+		}
+	</script>
+</head>
+<body>
+	<!-- inner text -->
+	<div id="innertext">
+		<fieldset style="padding:15px;">
+			<legend><b>Link New Specimen</b></legend>
+			<form name="adddupform" method="post" action="dupelist.php" onsubmit="return validateDupeForm(this)">
+				<div style="margin:3px;">
+					<b>Last Name:</b>
+					<input name="lastname" type="text" value="<?php echo $lastName; ?>" />
+				</div>
+				<div style="margin:3px;">
+					<b>Number:</b>
+					<input name="collnum" type="text" value="<?php echo $collNum; ?>" />
+				</div>
+				<div style="margin:3px;">
+					<b>Date:</b>
+					<input name="colldate" type="text" value="<?php echo $collDate; ?>" />
+				</div>
+				<div style="margin:3px;">
+					<b>Catalog Number:</b>
+					<input name="catnum" type="text" value="" />
+				</div>
+				<div style="margin:3px;">
+					<b>occid:</b>
+					<input name="occid" type="text" value="" />
+ 				</div>
+				<div style="margin:20px;">
+					<input name="curoccid" type="hidden" value="<?php echo $currentOccid; ?>" />
+					<input name="" type="submit" value="Search for Duplicates" />
+ 				</div>
+			</form>
+		</fieldset>
+		<fieldset>
+			<legend><b>Possible Duplicates</b></legend>
+			<?php 
+			if($dupArr){
+				foreach($dupArr as $dupOccid => $occArr){
+					?>
+					<div style="margin:30px 10px">
+						<div>
+							<?php 
+							echo $occArr['collname'];
+							?>
+						</div>
+						<div>
+							<?php 
+							echo $occArr['recordedby'].' '.$occArr['recordnumber'].' <span style="margin-left:15px">'.$occArr['eventdate'];
+							if($occArr['verbatimeventdate']) echo ' ('.$occArr['verbatimeventdate'].')';
+							echo '</span>';
+							echo '<span style="margin-left:50px">'.$occArr['catalognumber'].'</span>';
+							?>
+						</div>
+						<div>
+							<?php 
+							echo trim($occArr['country'].', '.$occArr['stateprovince'].', '.$occArr['county'].', '.$occArr['locality'],' ,');
+							?>
+						</div>
+						<div>
+							<a href="#" onclick="openIndWindow(<?php echo $dupOccid; ?>)">More Details</a>
+						</div>
+						<div style="margin:5px 0px 20px 15px;">
+							<form action="dupelist.php" method="post">
+								<input name="curoccid" type="hidden" value="<?php echo $currentOccid; ?>" />
+								<input name="dupeoccid" type="hidden" value="<?php echo $dupOccid; ?>" />
+								<input name="dupetitle" type="hidden" value="<?php echo $occArr['recordedby'].' '.$occArr['recordnumber'].' '.$occArr['eventdate']; ?>"  />
+								<input name="submitaction" type="submit" value="Link as Duplicate" />
+							</form>
+						</div>
+					</div>
+					<?php
+				}
+			}
+			else{
+				echo '<div style="margin:20px;font-weight:bold">No specimens found matching search criteria</div>';
+			}
+			?>
+		</fieldset>
+	</div>
+</body>
+</html>
