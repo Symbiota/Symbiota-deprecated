9cb25c23adf711a62bb3739e1c8d7053494dafee
Edward Gilbert <egbiodiversity@gmail.com>
2016-07-11 02:30:06 -0500

    Observation Submission Adjustments
    - Modifications to allow for submission of unidentified observations for
    request of identification by an expert
    - Adjustments to fmvoucher schema definition to allow for linking
    unidentified vouchers

--- collections/editor/observationsubmit.php
+++ collections/editor/observationsubmit.php
@@ -3,7 +3,7 @@
 include_once('../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ObservationSubmitManager.php');
 header("Content-Type: text/html; charset=".$CHARSET);
-if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/editor/observatoinsubmit.php?'.$_SERVER['QUERY_STRING']);
+if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/editor/observationsubmit.php?'.$_SERVER['QUERY_STRING']);
 
 $action = array_key_exists("action",$_REQUEST)?$_REQUEST["action"]:"";
 $collId  = array_key_exists("collid",$_REQUEST)?$_REQUEST["collid"]:0;
@@ -115,13 +115,13 @@ if($collMap){
 						<div style="float:left;">
 							Scientific Name:
 							<br/>
-							<input type="text" id="sciname" name="sciname" maxlength="250" tabindex="2" style="width:390px;background-color:lightyellow;" />
+							<input type="text" id="sciname" name="sciname" maxlength="250" style="width:390px;" />
 							<input type="hidden" id="tidtoadd" name="tidtoadd" value="" />
 						</div>
 						<div style="float:left;">
 							Author:
 							<br/>
-							<input type="text" name="scientificnameauthorship" maxlength="100" tabindex="0" style="" value="" />
+							<input type="text" name="scientificnameauthorship" maxlength="100" tabindex="100" />
 						</div>
 					</div>
 					<div style="clear:both;margin-left:10px;padding:3px 0px 0px 10px;">
@@ -135,23 +135,23 @@ if($collMap){
 							?>
 						</select>
 						<span style="margin-left:15px;">Family:</span>
-						<input name="family" type="text" size="30" maxlength="50" tabindex="0" />
+						<input name="family" type="text" size="30" maxlength="50" tabindex="101" />
 					</div>
 					<div style="clear:both;">
 						<div style="float:left;">
 							Observer:
 							<br/>
-							<input type="text" name="recordedby" maxlength="255" tabindex="14" style="width:250px;background-color:lightyellow;" value="<?php echo $recordedBy; ?>" />
+							<input type="text" name="recordedby" maxlength="255" style="width:250px;background-color:lightyellow;" value="<?php echo $recordedBy; ?>" />
 						</div>
 						<div style="float:left">
 							Number:
 							<br/>
-							<input type="text" name="recordnumber" maxlength="45" tabindex="16" style="width:80px;" title="Observer Number, if observer uses a numbering system " />
+							<input type="text" name="recordnumber" maxlength="45" style="width:80px;" title="Observer Number, if observer uses a numbering system " />
 						</div>
 						<div style="float:left;">
 							Date:
 							<br/>
-							<input type="text" id="eventdate" name="eventdate" tabindex="18" style="width:120px;background-color:lightyellow;" onchange="verifyDate(this);" title="format: yyyy-mm-dd" />
+							<input type="text" id="eventdate" name="eventdate" style="width:120px;background-color:lightyellow;" onchange="verifyDate(this);" title="format: yyyy-mm-dd" />
 						</div>
 						<div style="float:left;margin:15px 0px 0px 5px;cursor:pointer;" onclick="toggle('obsextradiv')">
 							<img src="../../images/editplus.png" style="width:15px;" />
@@ -160,23 +160,23 @@ if($collMap){
 					<div id="obsextradiv" style="clear:both;padding:3px 0px 0px 10px;margin-bottom:20px;display:none;">
 						<div style="clear:both;margin-top:5px;">
 							Associated Observers:<br />
-							<input type="text" name="associatedcollectors" tabindex="20" maxlength="255" style="width:530px;" value="" />
+							<input type="text" name="associatedcollectors" maxlength="255" style="width:530px;" value="" />
 						</div>
 						<div style="float:left;margin:3px 0px 0px 10px;">
 							Identified By:
-							<input type="text" name="identifiedby" maxlength="255" tabindex="6" style="" value="" />
+							<input type="text" name="identifiedby" maxlength="255" style="" value="" />
 						</div>
 						<div style="float:left;margin:3px 0px 0px 10px;">
 							Date Identified:
-							<input type="text" name="dateidentified" maxlength="45" tabindex="8" style="" value="" />
+							<input type="text" name="dateidentified" maxlength="45" style="" value="" />
 						</div>
 						<div style="clear:both;padding:3px 0px 0px 0px;" >
 							ID References:
-							<input type="text" name="identificationreferences" tabindex="10" style="width:450px;" title="cf, aff, etc" />
+							<input type="text" name="identificationreferences" style="width:450px;" title="cf, aff, etc" />
 						</div>
 						<div style="clear:both;padding:3px 0px 0px 0px;" >
 							ID Remarks:
-							<input type="text" name="taxonremarks" tabindex="12" style="width:500px;" value="" />
+							<input type="text" name="taxonremarks" style="width:500px;" value="" />
 						</div>
 					</div>
 				</fieldset>
@@ -184,71 +184,48 @@ if($collMap){
 					<legend><b>Locality</b></legend>
 					<div style="clear:both;">
 						<div style="float:left;">
-							Country
-							<br/>
-							<input type="text" name="country" tabindex="32" style="width:150px;background-color:lightyellow;" value="" />
-						</div>
-						<div style="float:left;">
-							State/Province
-							<br/>
-							<input type="text" name="stateprovince" tabindex="34" style="width:150px;background-color:lightyellow;" value="" />
-						</div>
-						<div style="float:left;">
-							County
-							<br/>
-							<input type="text" name="county" tabindex="36" style="width:150px;" value="" />
-						</div>
-					</div>
-					<div style="clear:both;margin:4px 0px 2px 0px;">
-						Locality:<br />
-						<input type="text" name="locality" tabindex="40" style="width:95%;background-color:lightyellow;" value="" />
-					</div>
-					<div style="clear:both;margin-bottom:5px;">
-						<input type="checkbox" name="localitysecurity" tabindex="42" style="" value="1" title="Hide Locality Data from General Public" />
-						Hide Locality Details from General Public (rare, threatened, or sensitive species)
-					</div>
-					<div style="clear:both;">
-						<div style="float:left;">
 							Latitude
 							<br/>
-							<input type="text" id="pointlat" name="decimallatitude" tabindex="44" maxlength="10" style="width:88px;background-color:lightyellow;" value="" onchange="verifyLatValue(this.form)" title="Decimal Format (eg 34.5436)" />
+							<input type="text" id="pointlat" name="decimallatitude" maxlength="10" style="width:88px;background-color:lightyellow;" value="" onchange="verifyLatValue(this.form)" title="Decimal Format (eg 34.5436)" />
 						</div>
 						<div style="float:left;">
 							Longitude
 							<br/>
-							<input type="text" id="pointlong" name="decimallongitude" tabindex="46" maxlength="13" style="width:88px;background-color:lightyellow;" value="" onchange="verifyLngValue(this.form)" title="Decimal Format (eg -112.5436)" />
-							<span style="margin:15px 5px 0px 5px;cursor:pointer;" onclick="openMappingAid('obsform','decimallatitude','decimallongitude');">
-								<img src="../../images/world.png" style="width:12px;" title="Coordinate Map Aid" />
+							<input type="text" id="pointlong" name="decimallongitude" maxlength="13" style="width:88px;background-color:lightyellow;" value="" onchange="verifyLngValue(this.form)" title="Decimal Format (eg -112.5436)" />
+							<span style="margin:15px 5px 0px 5px;">
+								<a href="#" onclick="openMappingAid('obsform','decimallatitude','decimallongitude');"><img src="../../images/world.png" style="width:12px;" title="Coordinate Map Aid" /></a>
 							</span>
-							<span style="margin:15px 2px 0px 2px;text-align:center;font-size:85%;font-weight:bold;color:maroon;background-color:#FFFFD7;padding:2px;border:1px outset #A0A0A0;cursor:pointer;" onclick="toggle('dmsdiv');">
-								DMS
+							<span style="margin:15px 5px 0px 2px;text-align:center;font-size:90%;font-weight:bold;color:maroon;background-color:#FFFFD7;padding:2px 3px;border:1px outset #A0A0A0;">
+								<a href="#" onclick="toggle('dmsdiv');">DMS</a>
 							</span>
 						</div>
 						<div style="float:left;">
 							Uncertainty(m)
 							<br/>
-							<input type="text" name="coordinateuncertaintyinmeters" tabindex="48" maxlength="10" style="width:80px;background-color:lightyellow;" onchange="inputIsNumeric(this, 'Lat/long uncertainty')" title="Uncertainty in Meters" />
+							<input type="text" name="coordinateuncertaintyinmeters" maxlength="10" style="width:80px;background-color:lightyellow;" onchange="inputIsNumeric(this, 'Lat/long uncertainty')" title="Uncertainty in Meters" />
 						</div>
 						<div style="float:left;">
 							Datum
 							<br/>
-							<input type="text" name="geodeticdatum" tabindex="50" maxlength="255" style="width:80px;" value="" />
+							<input type="text" name="geodeticdatum" maxlength="255" style="width:80px;" />
+						</div>
+						<div style="float:left;margin-left:5px;">
+							Country
+							<br/>
+							<input type="text" name="country" style="width:130px;background-color:lightyellow;" onchange="verifyPoliticalUnits(this.form)" />
 						</div>
 						<div style="float:left;">
-							Elev. (meters)
+							State/Province
 							<br/>
-							<input type="text" name="minimumelevationinmeters" tabindex="52" maxlength="6" style="width:85px;" value="" onchange="verifyElevValue(this)" title="Minumum Elevation In Meters" />
-							<span style="margin:15px 3px 0px 3px;text-align:center;font-weight:bold;color:maroon;background-color:#FFFFD7;padding:2px;border:1px outset #A0A0A0;cursor:pointer;" onclick="toggle('elevftdiv');">
-								ft.
-							</span>
+							<input type="text" name="stateprovince" style="width:130px;background-color:lightyellow;" onchange="verifyPoliticalUnits(this.form)" />
 						</div>
 						<div style="float:left;">
-							Georeference Remarks
+							County
 							<br/>
-							<input type="text" name="georeferenceremarks" tabindex="70" maxlength="255" style="width:250px;" value="" />
+							<input type="text" name="county" style="width:130px;" onchange="verifyPoliticalUnits(this.form)" />
 						</div>
 					</div>
-					<div id="dmsdiv" style="display:none;float:left;padding:15px;background-color:lightyellow;border:1px solid yellow;width:270px;">
+					<div id="dmsdiv" style="display:none;clear:both;margin:5px 0px 5px 50px;padding:15px;border:2px solid yellow;width:270px;">
 						<div>
 							Latitude: 
 							<input id="latdeg" style="width:35px;" title="Latitude Degree" />&deg; 
@@ -273,11 +250,34 @@ if($collMap){
 							<input type="button" value="Insert Lat/Long Values" onclick="insertLatLng(this.form)" />
 						</div>
 					</div>
-					<div id="elevftdiv" style="display:none;float:right;padding:15px;background-color:lightyellow;border:1px solid yellow;width:180px;margin:0px 160px 10px 0px;">
-						Elevation: 
-						<input id="elevft" style="width:45px;" /> feet
-						<div style="margin:5px;">
-							<input type="button" value="Insert Elevation" onclick="insertElevFt(this.form)" />
+					<div style="clear:both;margin:4px 0px 2px 0px;">
+						Locality:<br />
+						<input type="text" name="locality" style="width:95%;background-color:lightyellow;" value="" />
+					</div>
+					<div style="clear:both;margin-bottom:5px;">
+						<input type="checkbox" name="localitysecurity" tabindex="102" style="" value="1" title="Hide Locality Data from General Public" />
+						Hide Locality Details from General Public (rare, threatened, or sensitive species)
+					</div>
+					<div style="clear:both;">
+						<div style="float:left;">
+							Elev. (meters)
+							<br/>
+							<input type="text" name="minimumelevationinmeters" maxlength="6" style="width:85px;" value="" onchange="verifyElevValue(this)" title="Minumum Elevation In Meters" />
+							<span style="margin:15px 3px 0px 3px;text-align:center;font-weight:bold;color:maroon;background-color:#FFFFD7;padding:2px;border:1px outset #A0A0A0;cursor:pointer;" onclick="toggle('elevftdiv');">
+								ft.
+							</span>
+						</div>
+						<div id="elevftdiv" style="display:none;float:left;padding:15px;background-color:lightyellow;border:1px solid yellow;width:180px;margin:10px;">
+							Elevation: 
+							<input id="elevft" style="width:45px;" /> feet
+							<div style="margin:5px;">
+								<input type="button" value="Insert Elevation" onclick="insertElevFt(this.form)" />
+							</div>
+						</div>
+						<div style="float:left;">
+							Georeference Remarks
+							<br/>
+							<input type="text" name="georeferenceremarks" maxlength="255" style="width:375px;" value="" />
 						</div>
 					</div>
 				</fieldset>
@@ -285,35 +285,35 @@ if($collMap){
 					<legend><b>Misc</b></legend>
 					<div style="padding:3px;">
 						Habitat:
-						<input type="text" name="habitat" tabindex="82" style="width:600px;" value="" />
+						<input type="text" name="habitat" style="width:725px;" value="" />
 					</div>
 					<div style="padding:3px;">
 						Substrate:
-						<input type="text" name="substrate" tabindex="82" style="width:600px;" value="" />
+						<input type="text" name="substrate" style="width:715px;" value="" />
 					</div>
 					<div style="padding:3px;">
 						Associated Taxa:
-						<input type="text" name="associatedtaxa" tabindex="84" style="width:600px;background-color:" value="" />
+						<input type="text" name="associatedtaxa" style="width:680px;background-color:" value="" />
 					</div>
 					<div style="padding:3px;">
 						Description of Organism:
-						<input type="text" name="verbatimattributes" tabindex="86" style="width:600px;" value="" />
+						<input type="text" name="verbatimattributes" style="width:640px;" value="" />
 					</div>
 					<div style="padding:3px;">
 						General Notes:
-						<input type="text" name="occurrenceremarks" tabindex="88" style="width:600px;" value="" title="Occurrence Remarks" />
+						<input type="text" name="occurrenceremarks" style="width:685px;" value="" title="Occurrence Remarks" />
 					</div>
 					<div style="padding:3px;">
 						<span title="e.g. sterile, flw, frt, flw/frt ">
 							Reproductive Condition:
-							<input type="text" name="reproductivecondition" tabindex="98" maxlength="255" style="width:140px;" value="" />
+							<input type="text" name="reproductivecondition" maxlength="255" style="width:180px;" value="" />
 						</span>
-						<span style="margin-left:30px;" title="e.g. planted, seeded, garden excape, etc">
+						<span style="margin-left:15px;" title="e.g. planted, seeded, garden excape, etc">
 							Establishment Means:
-							<input type="text" name="establishmentmeans" tabindex="100" maxlength="32" style="width:140px;" value="" />
+							<input type="text" name="establishmentmeans" maxlength="32" style="width:180px;" value="" />
 						</span>
 						<span style="margin-left:15px;" title="Click if specimen was cultivated ">
-							<input type="checkbox" name="cultivationstatus" tabindex="102" style="" value="" />
+							<input type="checkbox" name="cultivationstatus" style="" value="" />
 							Cultivated
 						</span>
 					</div>
@@ -340,7 +340,7 @@ if($collMap){
 				?>
 				<fieldset>
 					<legend><b>Images</b></legend>
-					<div style='padding:10px;width:675px;border:1px solid yellow;background-color:FFFF99;'>
+					<div style='padding:10px;width:700px;border:1px solid yellow;background-color:FFFF99;'>
 				    	<!-- following line sets MAX_FILE_SIZE (must precede the file input field)  -->
 						<input type='hidden' name='MAX_FILE_SIZE' value='4000000' />
 						<div>
@@ -359,7 +359,7 @@ if($collMap){
 							<img src="../../images/add.png" style="width:15px;" />
 						</div>
 					</div>
-					<div id="img2div" style='padding:10px;width:675px;border:1px solid yellow;background-color:FFFF99;display:none;'>
+					<div id="img2div" style='padding:10px;width:700px;border:1px solid yellow;background-color:FFFF99;display:none;'>
 						<div>
 							Image 2: <input name="imgfile2" type="file" size="70" onchange="verifyImageSize(this)" />
 							<input type="button" value="Reset" onclick="document.obsform.imgfile2.value = ''">
@@ -376,7 +376,7 @@ if($collMap){
 							<img src="../../images/add.png" style="width:15px;" />
 						</div>
 					</div>
-					<div id="img3div" style='padding:10px;width:675px;border:1px solid yellow;background-color:FFFF99;display:none;'>
+					<div id="img3div" style='padding:10px;width:700px;border:1px solid yellow;background-color:FFFF99;display:none;'>
 						<div>
 							Image 3: <input name="imgfile3" type="file" size="70" onchange="verifyImageSize(this)" />
 							<input type="button" value="Reset" onclick="document.obsform.imgfile3.value = ''">
