59219842ce13e47b18072491a2c28261b49a812f
Edward Gilbert <egbiodiversity@gmail.com>
2016-08-15 23:15:25 -0700

    Minor ImageShared adjustment
    - adjustment to image url test fucntion so that it can handle image
    server redirects

--- classes/ImageShared.php
+++ classes/ImageShared.php
@@ -1096,10 +1096,11 @@ class ImageShared{
 				$exists = false;
 		    }
 		    curl_setopt($handle, CURLOPT_HEADER, false);
-		    curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+		    curl_setopt($handle, CURLOPT_FAILONERROR, true); 
 		    curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") ); // request as if Firefox   
 		    curl_setopt($handle, CURLOPT_NOBODY, true);
 		    curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+		    curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
 		    $exists = curl_exec($handle);
 		    curl_close($handle);
 	    }
