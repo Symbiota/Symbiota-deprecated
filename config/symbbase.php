7e61746afcc8c9be26ee420a89c17260d689fb9e
bdbrandt <benjamin.brandt@nau.edu>
2017-04-10 05:03:07 -0500

    Merge correction to previous commit.

--- config/symbbase.php
+++ config/symbbase.php
@@ -52,7 +52,7 @@ if(isset($_SESSION['userrights'])){
     $USER_RIGHTS = $_SESSION['userrights'];
 }
 
-$CSS_VERSION = 'ver=201703';
+$CSS_VERSION = '20170409';
 $USER_DISPLAY_NAME = (array_key_exists("dn",$PARAMS_ARR)?$PARAMS_ARR["dn"]:"");
 $USERNAME = (array_key_exists("un",$PARAMS_ARR)?$PARAMS_ARR["un"]:0);
 $SYMB_UID = (array_key_exists("uid",$PARAMS_ARR)?$PARAMS_ARR["uid"]:0);
