849fe135b577cc1e8d5f42f344c2fac519e0ab21
Edward Gilbert <egbiodiversity@gmail.com>
2017-08-07 21:43:58 -0500

    Minor bug fix within observation submit

--- collections/editor/observationsubmit.php
+++ collections/editor/observationsubmit.php
@@ -3,7 +3,7 @@
 include_once('../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ObservationSubmitManager.php');
 header("Content-Type: text/html; charset=".$CHARSET);
-if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/editor/observatoinsubmit.php?'.$_SERVER['QUERY_STRING']);
+if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/editor/observationsubmit.php?'.$_SERVER['QUERY_STRING']);
 
 $action = array_key_exists("action",$_REQUEST)?$_REQUEST["action"]:"";
 $collId  = array_key_exists("collid",$_REQUEST)?$_REQUEST["collid"]:0;
