dd67c1f450256123797fe4de46b2d1e260b8ad94
Edward Gilbert <egbiodiversity@gmail.com>
2017-10-25 09:36:11 -0500

    Slideshow Development
    - Convert slideshow to have multi-lanuage capabilities

--- content/lang/index.es.php
+++ content/lang/index.es.php
@@ -8,3 +8,9 @@ Language: Español (Spanish)
 //Various plugin variables
 $LANG['QSEARCH_SEARCH'] = 'B&uacute;squeda de especies';
 $LANG['QSEARCH_SEARCH_BUTTON'] = 'B&uacute;squeda';
+
+//Slideshow plugin variables
+$LANG['HIDE_CAPTION'] = 'OCULTAR SUBT&Iacute;TULO';
+$LANG['SHOW_CAPTION'] = 'MOSTRAR T&Iacute;TULO';
+$LANG['IMAGE_BY'] = 'Imagen por';
+$LANG['COURTESY_OF'] = 'Cortes&iacute;a de';
