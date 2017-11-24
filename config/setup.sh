2633a5f0aafa98f710b92cf4f85a701f2d14e98e
Edward Gilbert <egbot@asu.edu>
2017-11-24 15:10:16 -0500

    Adjustments to setup file

--- config/setup.sh
+++ config/setup.sh
@@ -1,20 +1,31 @@
+#Create configuration files from conf template files
 echo "Creating dbconfiguration file: /config/dbconnection.php"
-mv ../config/dbconnection_template.php ../config/dbconnection.php
+cp ../config/dbconnection_template.php ../config/dbconnection.php
 echo "Creating Symbiota configuration file: /config/symbini.php"
-mv ../config/symbini_template.php ../config/symbini.php
+cp ../config/symbini_template.php ../config/symbini.php
 echo "Creating homepage: /index.php"
-mv ../index_template.php ../index.php
+cp ../index_template.php ../index.php
 echo "Creating header include: /header.php"
-mv ../header_template.php ../header.php
+cp ../header_template.php ../header.php
 echo "Creating Left Menu include: /leftmenu.php"
-mv ../leftmenu_template.php ../leftmenu.php
+cp ../leftmenu_template.php ../leftmenu.php
 echo "Creating footer include: /footer.php"
-mv ../footer_template.php ../footer.php
+cp ../footer_template.php ../footer.php
 echo "Creating central CSS: /css/main.css"
-mv ../css/main_template.css ../css/main.css
+cp ../css/main_template.css ../css/main.css
 echo "Creating CSS for Taxon Profile page: /css/speciesprofile.css"
-mv ../css/speciesprofile_template.css ../css/speciesprofile.css
+cp ../css/speciesprofile_template.css ../css/speciesprofile.css
 echo "Creating default JQuery CSS: /css/jquery-ui.css"
-mv ../css/jquery-ui_template.css ../css/jquery-ui.css
+cp ../css/jquery-ui_template.css ../css/jquery-ui.css
 echo "Creating default Data Usage Policy page: /misc/usagepolicy_template.php"
-mv ../misc/usagepolicy_template.php ../misc/usagepolicy.php
+cp ../misc/usagepolicy_template.php ../misc/usagepolicy.php
+
+#Adjust file permission to give write access to certain folders and files
+chmod 777 ../webservices/dwc/rss.xml
+chmod -R 777 ../temp
+chmod -R 777 ../content/collicon
+chmod -R 777 ../content/css
+chmod -R 777 ../content/dwca
+chmod -R 777 ../content/geolocate
+chmod -R 777 ../content/lang
+chmod -R 777 ../content/logs 
