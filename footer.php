e94ea907033c8279ab82413a545a64a0d22a2ca5
bdbrandt <benjamin.brandt@nau.edu>
2017-11-26 13:45:53 -0600

    Initial commit of Oregon Flora Project files.

---
+++ footer.php
@@ -0,0 +1,55 @@
+<div class="footer-bottom">
+	<div class="wrap">
+		<div class="section group">
+			<div class="col_1_of_5 span_1_of_5">
+				<a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/OSUHlogo.png" class="f-logo" alt=""/></a>
+			</div>
+			<div class="col_1_of_5 span_1_of_5">
+				<p>Oregon Flora Project</p>
+				<p>Dept. Botany & Plant Pathology</p>
+				<p>Oregon State University</p>
+				<p>Corvallis, OR 97331-2902</p>
+			</div>
+			<div class="col_1_of_5 span_1_of_5">
+				<ul class="list">
+					<li><a href="<?php echo $clientRoot; ?>/collections/index.php">Search Collections</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/collections/map/mapinterface.php" target="_blank" >Map Search</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/imagelib/search.php">Image Search</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/imagelib/index.php">Browse Images</a></li>
+				</ul>
+				<ul class="list1">
+					<li><a href="<?php echo $clientRoot; ?>/taxa/admin/taxonomydynamicdisplay.php">Taxonomy Explorer</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/projects/index.php">Inventories</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=checklist">Dynamic Checklist</a></li>
+					<li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key">Dynamic Key</a></li>
+				</ul>
+			</div>
+			<div class="col_1_of_5 span_1_of_5">
+				<form action="https://www.paypal.com/cgi-bin/webscr" style="float:right;" method="post" target="_blank">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="ELVFJLHX3T9JU">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to support online!" title="Support Oregon Flora Project">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+
+		var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear'
+		};
+
+	});
+</script>
+<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"></span></a>
+<script src="<?php echo $clientRoot; ?>/js/jquery.scrollTo.js"></script>
+<script src="<?php echo $clientRoot; ?>/js/classie.js"></script>
+<script src="<?php echo $clientRoot; ?>/js/cbpAnimatedHeader.min.js"></script>
+</div>
