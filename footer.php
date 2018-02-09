1d0aeaa2de4a6d6bb6bca5ad3b6a8ef71b53e125
Tom Kerns <tom@tundratech.net>
2018-02-08 22:43:10 -0900

    Create initial markup and css for footer.

--- footer.php
+++ footer.php
@@ -1,41 +1,35 @@
 </div><!-- .content-wrapper -->
-<div class="footer-bottom">
-	<div class="wrap">
-		<div class="section group">
-			<div class="col_1_of_5 span_1_of_5">
-				<a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/OSUHlogo.png" class="f-logo" alt=""/></a>
-			</div>
-			<div class="col_1_of_5 span_1_of_5">
-				<p>Oregon Flora Project</p>
-				<p>Dept. Botany & Plant Pathology</p>
-				<p>Oregon State University</p>
-				<p>Corvallis, OR 97331-2902</p>
-			</div>
-			<div class="col_1_of_5 span_1_of_5">
-				<ul class="list">
-					<li><a href="<?php echo $clientRoot; ?>/collections/index.php">Search Collections</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/collections/map/mapinterface.php" target="_blank" >Map Search</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/imagelib/search.php">Image Search</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/imagelib/index.php">Browse Images</a></li>
-				</ul>
-				<ul class="list1">
-					<li><a href="<?php echo $clientRoot; ?>/taxa/admin/taxonomydynamicdisplay.php">Taxonomy Explorer</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/projects/index.php">Inventories</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=checklist">Dynamic Checklist</a></li>
-					<li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key">Dynamic Key</a></li>
-				</ul>
-			</div>
-			<div class="col_1_of_5 span_1_of_5">
-				<form action="https://www.paypal.com/cgi-bin/webscr" style="float:right;" method="post" target="_blank">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="ELVFJLHX3T9JU">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to support online!" title="Support Oregon Flora Project">
-					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-				</form>
-			</div>
-			<div class="clear"></div>
-		</div>
-	</div>
+<div class="footer-nav-wrapper">
+    <ul class="footer-nav">
+        <li><a href="<?php echo $clientRoot; ?>/contact.php">Contact</a></li>
+        <li><a href="<?php echo $clientRoot; ?>/disclaimer.php">Disclaimer</a></li>
+        <li><a href="<?php echo $clientRoot; ?>/portal/sitemap.php">Sitemap</a></li>
+    </ul>
+</div>
+<div class="footer-wrapper">
+    <div class="footer clearfix">
+        <div class="footer-left">
+            <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/OSUHlogo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
+            <p>Oregon Flora Project<br />
+            Dept. Botany & Plant Pathology<br />
+            Oregon State University<br />
+            Corvallis, OR 97331-2902</p>
+            <div class="copyright">
+                copyright &copy; OregonFlora, Oregon State University<br />
+                website by <a href="http://www.rareheron.com" target="_blank">Rareheron Web Design</a>, Portland, OR
+            </div>
+        </div>
+        <div class="footer-right">
+            The Oregon Flora Project is based at Oregon State University.<br>
+            Our program is wholly funded through grants and contributions.  We welcome your support!
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="ELVFJLHX3T9JU">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to support online!" title="Support Oregon Flora Project">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
+        </div>
+    </div>
 </div>
 <a href="#" id="to-top"><i class="fa fa-chevron-up"></i></a>
 <script src="<?php echo $clientRoot; ?>/js/navigation.js"></script>
