f3566681d5630fef76585dca6bb0f2275489e432
Tom Kerns <tom@tundratech.net>
2018-02-10 22:05:13 -0900

    Update footer

--- footer.php
+++ footer.php
@@ -4,30 +4,30 @@
         <li><a href="<?php echo $clientRoot; ?>/contact.php">Contact</a></li>
         <li><a href="<?php echo $clientRoot; ?>/disclaimer.php">Disclaimer</a></li>
         <li><a href="<?php echo $clientRoot; ?>/portal/sitemap.php">Sitemap</a></li>
+        <li><a href="<?php echo $clientRoot; ?>/site-feedback.php">Site Feedback</a></li>
     </ul>
 </div>
 <div class="footer-wrapper">
     <div class="footer clearfix">
         <div class="footer-left">
-            <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/OSUHlogo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
-            <p>Oregon Flora Project<br />
-            Dept. Botany & Plant Pathology<br />
-            Oregon State University<br />
-            Corvallis, OR 97331-2902</p>
+            <div class="footer-logos">
+                <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/layout/osu-logo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
+                <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/layout/symbiota-logo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
+            </div>
+            <p>Oregon Flora Project<br>
+            Dept. Botany & Plant Pathology<br>
+            Oregon State University<br>
+            Corvallis, OR 97331-2902<br>
+                <a href="mailto:info@oregonflora.org">info@oregonflora.org</a></p>
             <div class="copyright">
-                copyright &copy; OregonFlora, Oregon State University<br />
+                copyright &copy; OregonFlora, Oregon State University<br>
                 website by <a href="http://www.rareheron.com" target="_blank">Rareheron Web Design</a>, Portland, OR
             </div>
         </div>
         <div class="footer-right">
-            The Oregon Flora Project is based at Oregon State University.<br>
-            Our program is wholly funded through grants and contributions.  We welcome your support!
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-                <input type="hidden" name="cmd" value="_s-xclick">
-                <input type="hidden" name="hosted_button_id" value="ELVFJLHX3T9JU">
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to support online!" title="Support Oregon Flora Project">
-                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-            </form>
+            <p>The Oregon Flora Project is based at Oregon State University.<br>
+            Our program is wholly funded through grants and contributions.  We welcome your support!</p>
+            <a href="/donate.php" class="btn green-btn">Donate Now</a>
         </div>
     </div>
 </div>
