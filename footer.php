21e68990f3350a090b3ffcb17cfe6f81bb85f770
Tom Kerns <tom@tundratech.net>
2018-02-11 19:45:20 -0900

    Add responsive functionality to home page content.

--- footer.php
+++ footer.php
@@ -8,20 +8,17 @@
     </ul>
 </div>
 <div class="footer-wrapper">
-    <div class="footer clearfix">
+    <div class="footer">
         <div class="footer-left">
-            <div class="footer-logos">
+            <div class="col1">
                 <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/layout/osu-logo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
                 <a href="http://oregonstate.edu/dept/botany/herbarium/index.php" target="_blank" ><img src="<?php echo $clientRoot; ?>/images/layout/symbiota-logo.png"  alt="Oregon State University, Oregon Flora Project"/></a>
             </div>
-            <p>Oregon Flora Project<br>
+            <div class="col2">Oregon Flora Project<br>
             Dept. Botany & Plant Pathology<br>
             Oregon State University<br>
             Corvallis, OR 97331-2902<br>
-                <a href="mailto:info@oregonflora.org">info@oregonflora.org</a></p>
-            <div class="copyright">
-                copyright &copy; OregonFlora, Oregon State University<br>
-                website by <a href="http://www.rareheron.com" target="_blank">Rareheron Web Design</a>, Portland, OR
+                <a href="mailto:info@oregonflora.org">info@oregonflora.org</a>
             </div>
         </div>
         <div class="footer-right">
@@ -29,6 +26,9 @@
             Our program is wholly funded through grants and contributions.  We welcome your support!</p>
             <a href="/donate.php" class="btn green-btn">Donate Now</a>
         </div>
+    </div>
+    <div class="copyright inner-content">
+        website by <a href="http://www.rareheron.com" target="_blank">Rareheron Web Design</a>, Portland, OR
     </div>
 </div>
 <a href="#" id="to-top"><i class="fa fa-chevron-up"></i></a>
