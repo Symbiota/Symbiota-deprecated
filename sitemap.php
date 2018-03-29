5fa2d6f911e613cfff1e39c1f177a7d42b4a5288
Edward Gilbert <egbot@asu.edu>
2018-03-29 12:23:26 -0500

    Admin Functions
    - Remove batch taxonomy cleaning function option

--- sitemap.php
+++ sitemap.php
@@ -3,7 +3,7 @@ include_once('config/symbini.php');
 include_once($SERVER_ROOT.'/classes/SiteMapManager.php');
 include_once($SERVER_ROOT.'/content/lang/sitemap.'.$LANG_TAG.'.php');
 header("Content-Type: text/html; charset=".$CHARSET);
-$submitAction = array_key_exists('submitaction',$_REQUEST)?$_REQUEST['submitaction']:''; 
+$submitAction = array_key_exists('submitaction',$_REQUEST)?$_REQUEST['submitaction']:'';
 
 $smManager = new SiteMapManager();
 ?>
@@ -33,9 +33,9 @@ $smManager = new SiteMapManager();
 		echo " <b>".$LANG['SITEMAP']."</b>";
 		echo "</div>";
 	}
-		
-	?> 
-	<!-- This is inner text! --> 
+
+	?>
+	<!-- This is inner text! -->
 	<div id="innertext">
 		<h1><?php echo $LANG['SITEMAP']; ?></h1>
 		<div style="margin:10px;">
@@ -48,15 +48,15 @@ $smManager = new SiteMapManager();
 				<li><?php echo (isset($LANG['DATA_PUBLISHING'])?$LANG['DATA_PUBLISHING']:'Data Publishing');?></li>
 				<li style="margin-left:15px"><a href="collections/datasets/rsshandler.php" target="_blank"><?php echo $LANG['COLLECTIONS_RSS'];?></a></li>
 				<li style="margin-left:15px"><a href="collections/datasets/datapublisher.php"><?php echo $LANG['DARWINCORE'];?></a> - <?php echo $LANG['PUBDATA'];?></li>
-				<?php 
+				<?php
 				if(file_exists('webservices/dwc/rss.xml')){
 					echo '<li style="margin-left:15px;"><a href="webservices/dwc/rss.xml" target="_blank">'.$LANG['RSS'].'</a></li>';
 				}
 				?>
 				<li><a href="collections/misc/rarespecies.php"><?php echo $LANG['RARESPEC'];?></a> - <?php echo $LANG['LISTOFTAXA'];?></li>
-				
+
 			</ul>
-				
+
 			<div style="margin-top:10px;"><h2><?php echo $LANG['IMGLIB'];?></h2></div>
 			<ul>
 				<li><a href="imagelib/index.php"><?php echo $LANG['IMGLIB'];?></a></li>
@@ -64,14 +64,14 @@ $smManager = new SiteMapManager();
 				<li><a href="imagelib/contributors.php"><?php echo $LANG['CONTRIB'];?></a></li>
 				<li><a href="misc/usagepolicy.php"><?php echo $LANG['USAGEPOLICY'];?></a></li>
 			</ul>
-			
+
 			<div style="margin-top:10px;"><h2><?php echo $LANG['TAXONOMY'];?></h2></div>
 			<ul>
 				<li><a href="taxa/taxonomy/taxonomydisplay.php"><?php echo $LANG['TAXTREE'];?></a></li>
 				<li><a href="taxa/taxonomy/taxonomydynamicdisplay.php"><?php echo $LANG['DYNTAXTREE'];?></a></li>
 			</ul>
 
-			<?php 
+			<?php
 			$clList = $smManager->getChecklistList((array_key_exists('ClAdmin',$USER_RIGHTS)?$USER_RIGHTS['ClAdmin']:0));
 			$clAdmin = array();
 			if($clList && isset($USER_RIGHTS['ClAdmin'])){
@@ -104,7 +104,7 @@ $smManager = new SiteMapManager();
 
 			<fieldset style="margin:30px 0px 10px 10px;padding-left:25px;padding-right:15px;">
 				<legend><b><?php echo $LANG['MANAGTOOL'];?></b></legend>
-				<?php 
+				<?php
 				if($SYMB_UID){
 					if($IS_ADMIN){
 						?>
@@ -115,9 +115,6 @@ $smManager = new SiteMapManager();
 							</li>
 							<li>
 								<a href="profile/usertaxonomymanager.php"><?php echo $LANG['TAXINTER'];?></a>
-							</li>  
-							<li>
-								<a href="collections/cleaning/taxonomycleaner.php"><?php echo isset($LANG['TAXONOMIC_CLEANER'])?$LANG['TAXONOMIC_CLEANER']:'Global Taxonomic Name Cleaner'; ?></a>
 							</li>
 							<li>
 								<a href="<?php echo $CLIENT_ROOT; ?>/collections/misc/collmetadata.php">
@@ -142,38 +139,38 @@ $smManager = new SiteMapManager();
 						</ul>
 						<?php
 					}
-					
-					if($KEY_MOD_IS_ACTIVE || array_key_exists("KeyAdmin",$USER_RIGHTS)){ 
+
+					if($KEY_MOD_IS_ACTIVE || array_key_exists("KeyAdmin",$USER_RIGHTS)){
 						?>
 						<h3><?php echo $LANG['IDKEYS'];?></h3>
-						<?php 
+						<?php
 						if(!$KEY_MOD_IS_ACTIVE && array_key_exists("KeyAdmin",$USER_RIGHTS)){
 							?>
 							<div style="color:red;margin-left:10px;">
                                 <?php echo $LANG['KEYMODULE'];?>
 							</div>
-							<?php 
+							<?php
 						}
 						?>
 						<ul>
-							<?php 
+							<?php
 							if($IS_ADMIN || array_key_exists("KeyAdmin",$USER_RIGHTS)){
 								?>
 								<li>
                                     <?php echo $LANG['AUTHOKEY'];?> <a href="<?php echo $CLIENT_ROOT; ?>/ident/admin/index.php"><?php echo $LANG['CHARASTATES'];?></a>
 								</li>
-								<?php 
+								<?php
 							}
-							if($IS_ADMIN || array_key_exists("KeyEditor",$USER_RIGHTS) || array_key_exists("KeyAdmin",$USER_RIGHTS)){ 
+							if($IS_ADMIN || array_key_exists("KeyEditor",$USER_RIGHTS) || array_key_exists("KeyAdmin",$USER_RIGHTS)){
 								?>
 								<li>
                                     <?php echo $LANG['AUTHIDKEY'];?>
 								</li>
 								<li>
-                                    <?php 
+                                    <?php
 									//Show Checklists that user has explicit editing rights
 									if($clAdmin){
-	                                    echo $LANG['CODINGCHARA'].'<br/>'; 
+	                                    echo $LANG['CODINGCHARA'].'<br/>';
 										echo '<ul>';
 										foreach($clAdmin as $vClid => $name){
 											echo "<li><a href='".$CLIENT_ROOT."/ident/tools/massupdate.php?clid=".$vClid."'>".$name."</a></li>";
@@ -187,7 +184,7 @@ $smManager = new SiteMapManager();
 							else{
 								?>
 								<li><?php echo $LANG['NOTAUTHIDKEY'];?></li>
-								<?php 
+								<?php
 							}
 							?>
 						</ul>
@@ -196,17 +193,17 @@ $smManager = new SiteMapManager();
 					?>
 					<h3><?php echo $LANG['IMAGES'];?></h3>
 					<div style="margin:10px;">
-                        <?php echo $LANG['SEESYMBDOC'];?> 
+                        <?php echo $LANG['SEESYMBDOC'];?>
 						<a href="http://symbiota.org/docs/image-submission-2/"><?php echo $LANG['IMGSUB'];?></a>
                         <?php echo $LANG['FORANOVERVIEW'];?>
 					</div>
 					<ul>
-						<?php 
-						if($IS_ADMIN || array_key_exists('TaxonProfile',$USER_RIGHTS)){ 
+						<?php
+						if($IS_ADMIN || array_key_exists('TaxonProfile',$USER_RIGHTS)){
 							?>
 							<li>
 								<a href="taxa/profile/tpeditor.php?tabindex=1" target="_blank">
-                                    <?php echo $LANG['BASICFIELD'];?> 
+                                    <?php echo $LANG['BASICFIELD'];?>
 								</a>
 							</li>
 							<?php
@@ -218,14 +215,14 @@ $smManager = new SiteMapManager();
 	                                <?php echo $LANG['IMGOBSER'];?>
 								</a>
 							</li>
-							<?php 
+							<?php
 						}
 						?>
 					</ul>
 
 					<h3><?php echo $LANG['BIOINV'];?></h3>
 					<ul>
-						<?php 
+						<?php
 						if($IS_ADMIN){
 							echo '<li><a href="projects/index.php?newproj=1">'.$LANG['ADDNEWPROJ'].'</a></li>';
 							if($projList){
@@ -247,7 +244,7 @@ $smManager = new SiteMapManager();
 					</ul>
 
 					<h3><?php echo $LANG['TAXONPROF'];?></h3>
-					<?php 
+					<?php
 					if($IS_ADMIN || array_key_exists("TaxonProfile",$USER_RIGHTS)){
 						?>
 						<div style="margin:10px;">
@@ -260,29 +257,29 @@ $smManager = new SiteMapManager();
 							<li style="margin-left:15px;"><a href="taxa/profile/tpeditor.php?taxon=&category=imagequicksort&tabindex=2"><?php echo $LANG['IMGSORTORD'];?></a></li>
 							<li style="margin-left:15px;"><a href="taxa/profile/tpeditor.php?taxon=&category=imageadd&tabindex=3"><?php echo $LANG['ADDNEWIMG'];?></a></li>
 						</ul>
-						<?php 
+						<?php
 					}
 					else{
 						?>
 						<ul>
 							<li><?php echo $LANG['NOTAUTHOTAXONPAGE'];?></li>
 						</ul>
-						<?php 
+						<?php
 					}
 					?>
 					<h3><?php echo $LANG['TAXONOMY'];?></h3>
 					<ul>
-						<?php 
+						<?php
 						if($IS_ADMIN || array_key_exists("Taxonomy",$USER_RIGHTS)){
 							?>
 							<li><?php echo $LANG['EDITTAXPL'];?> <a href="taxa/taxonomy/taxonomydisplay.php"><?php echo $LANG['TAXTREEVIEW'];?></a></li>
 							<li><a href="taxa/taxonomy/taxonomyloader.php"><?php echo $LANG['ADDTAXANAME'];?></a></li>
 							<li><a href="taxa/taxonomy/taxaloader.php"><?php echo $LANG['BATCHTAXA'];?></a></li>
-							<?php 
+							<?php
 							if($IS_ADMIN || array_key_exists("Taxonomy",$USER_RIGHTS)){
 								?>
 								<li><a href="taxa/profile/eolmapper.php"><?php echo $LANG['EOLLINK'];?></a></li>
-								<?php 
+								<?php
 							}
 						}
 						else{
@@ -296,7 +293,7 @@ $smManager = new SiteMapManager();
                         <?php echo $LANG['TOOLSFORMANAGE'];?>.
 					</div>
 					<ul>
-						<?php 
+						<?php
 						if($clAdmin){
 							foreach($clAdmin as $k => $v){
 								echo "<li><a href='".$CLIENT_ROOT."/checklists/checklist.php?cl=".$k."&emode=1'>$v</a></li>";
@@ -308,7 +305,7 @@ $smManager = new SiteMapManager();
 						?>
 					</ul>
 
-					<?php 
+					<?php
 					if(isset($ACTIVATE_EXSICCATI) && $ACTIVATE_EXSICCATI){
 						?>
 						<h3><?php echo $LANG['EXSICCATII'];?></h3>
@@ -318,7 +315,7 @@ $smManager = new SiteMapManager();
 						<ul>
 							<li><a href="collections/exsiccati/index.php"><?php echo $LANG['EXSICC'];?></a></li>
 						</ul>
-						<?php 
+						<?php
 					}
 					?>
 
@@ -331,7 +328,7 @@ $smManager = new SiteMapManager();
                             <?php echo $LANG['COLLLIST'];?>
 						</div>
 						<ul>
-						<?php 
+						<?php
 						$smManager->setCollectionList();
 						if($collList = $smManager->getCollArr()){
 							foreach($collList as $k => $cArr){
@@ -355,7 +352,7 @@ $smManager = new SiteMapManager();
 						<a href="http://symbiota.org/docs/specimen-data-management/" target="_blank"><?php echo $LANG['SYMBDOCU'];?></a> <?php echo $LANG['FORMOREINFO'];?>.
 					</div>
 					<div style="margin:10px;">
-						<?php 
+						<?php
 						$obsList = $smManager->getObsArr();
 						$genObsList = $smManager->getGenObsArr();
 						$obsManagementStr = '';
@@ -364,7 +361,7 @@ $smManager = new SiteMapManager();
                             <?php echo $LANG['OIVS'];?>
 						</div>
 						<ul>
-							<?php 
+							<?php
 							if($obsList){
 								foreach($genObsList as $k => $oArr){
 									?>
@@ -393,13 +390,13 @@ $smManager = new SiteMapManager();
 							?>
 						</ul>
 						<?php
-						if($genObsList){ 
+						if($genObsList){
 							?>
 							<div style="font-weight:bold;">
                                 <?php echo $LANG['PERSONAL'];?>
 							</div>
 							<ul>
-								<?php 
+								<?php
 								foreach($genObsList as $k => $oArr){
 									?>
 									<li>
@@ -407,7 +404,7 @@ $smManager = new SiteMapManager();
 											<?php echo $oArr['name']; ?>
 										</a>
 									</li>
-									<?php 
+									<?php
 								}
 								?>
 							</ul>
@@ -421,11 +418,11 @@ $smManager = new SiteMapManager();
 							<ul>
 								<?php echo $obsManagementStr; ?>
 							</ul>
-						<?php 
+						<?php
 						}
 					?>
 					</div>
-					<?php 
+					<?php
 				}
 				else{
 					echo ''.$LANG['PLEASE'].' <a href="'.$CLIENT_ROOT.'/profile/index.php?refurl=../sitemap.php">'.$LANG['LOGIN'].'</a>'.$LANG['TOACCESS'].'<br/>'.$LANG['CONTACTPORTAL'].'.';
@@ -443,6 +440,6 @@ $smManager = new SiteMapManager();
 	</div>
 	<?php
 	include($SERVER_ROOT.'/footer.php');
-	?> 
+	?>
 </body>
 </html>
