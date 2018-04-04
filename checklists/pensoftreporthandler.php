f557a5641ca5b1865172fa62658e33d4ec13f5dc
Edward Gilbert <egbot@asu.edu>
2018-04-04 14:47:04 -0500

    Pensoft Checklist Publishing dev
    - Beginning code for automating generation of Pensoft Checklist format

--- checklists/pensoftreporthandler.php
+++ checklists/pensoftreporthandler.php
@@ -1,13 +1,13 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ChecklistVoucherAdmin.php');
-require $SERVER_ROOT.'/classes/PhpSpreadsheet/autoload.php';
+require_once($SERVER_ROOT.'/vendor/autoload.php');
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $clid = $_REQUEST['clid'];
-$rType = $_REQUEST['rtype'];
+//$rType = $_REQUEST['rtype'];
 
 $vManager = new ChecklistVoucherAdmin();
 $vManager->setClid($clid);
@@ -16,11 +16,14 @@ $vManager->setCollectionVariables();
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$clArr = $vManager->getPensoftChecklistArr();
+$penArr = $vManager->getPensoftArr();
+$headerArr = $penArr['header'];
+$taxaArr = $penArr['taxa'];
 
 $letters = range('A', 'Z');
-foreach($clArr as $rowCnt => $recArr){
+foreach($clArr as $tid => $recArr){
 	foreach($recArr as $cellCnt => $cellValue){
+		echo $letters[$cellCnt].$rowCnt.' - '.$cellValue.'<br/>';
 		$sheet->setCellValue($letters[$cellCnt].$rowCnt, $cellValue);
 	}
 }
