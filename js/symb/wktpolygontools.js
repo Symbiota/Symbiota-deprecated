caaa22d7d17078586ba865736b6c2b686259e212
Edward Gilbert <egbot@asu.edu>
2018-10-27 00:13:38 -0700

    Polygon Management tool development
    - Add reformatting tool to covert coordinate tuple polygons used within NEON KLM files to wktfootprint polygons that can be queried against MySQL spatial indexes
    - Fixed bug that was adding duplicate trailing points after each open, reformat, and save
    - Fix bug that failed to reformat strings with trailing whitespace
    - Improved extent of reformatting tool

--- js/symb/wktpolygontools.js
+++ js/symb/wktpolygontools.js
@@ -1,6 +1,6 @@
 function reformCoordinates(f){
 	var footprintWkt = trimPolygon(f.footprintwkt.value);
-	footprintWkt = validatePolygon(footprintWkt,true);
+	footprintWkt = validatePolygon(footprintWkt);
 	f.footprintwkt.value = footprintWkt;
 }
 
@@ -15,7 +15,7 @@ function validatePolygon(footprintWktInput){
 			for(i in footPolyArr){
 				var keys = Object.keys(footPolyArr[i]);
 				if(!isNaN(footPolyArr[i][keys[0]]) && !isNaN(footPolyArr[i][keys[1]])){
-					newStr = newStr + ', ' + parseFloat(footPolyArr[i][keys[0]]).toFixed(6) + " " + parseFloat(footPolyArr[i][keys[1]]).toFixed(6);
+					newStr = newStr + "," + parseFloat(footPolyArr[i][keys[0]]).toFixed(6) + " " + parseFloat(footPolyArr[i][keys[1]]).toFixed(6);
 				}
 				else{
 					alert("The footprint is not in the proper format. Please recreate it using the map tools.");
@@ -29,6 +29,18 @@ function validatePolygon(footprintWktInput){
 		}
 	}
 
+	//Check to see if input is a KML coordinate tuple (e.g. -99.238545,47.148081,0 -99.238545,47.148081,0 ...)
+	var patt = new RegExp(/^[\d-\.]+,[\d-\.]+,[\d]+\s+/);
+	if(patt.test(footprintWkt)){
+		var newStr = ''
+		var klmArr = footprintWkt.split(" ");
+		for(var i=0; i < klmArr.length; i++){
+			var pArr = klmArr[i].split(",");
+			newStr = newStr + "," + parseFloat(pArr[1]).toFixed(6) + " " + parseFloat(pArr[0]).toFixed(6);
+		}
+		footprintWkt = newStr.substr(1);
+	}
+
 	//Check to see if it's a GeoLocate polygon (e.g. 31.6661680128,-110.709762938,31.6669780128,-110.710163938,...)
 	var patt = new RegExp(/^[\d-\.]+,[\d-\.]+/);
 	if(patt.test(footprintWkt)){
@@ -36,7 +48,7 @@ function validatePolygon(footprintWktInput){
 		var coordArr = footprintWkt.split(",");
 		for(var i=0; i < coordArr.length; i++){
 			if((i % 2) == 1){
-				newStr = newStr + ", " + parseFloat(coordArr[i-1]).toFixed(6) + " " + parseFloat(coordArr[i]).toFixed(6);
+				newStr = newStr + "," + parseFloat(coordArr[i-1]).toFixed(6) + " " + parseFloat(coordArr[i]).toFixed(6);
 			}
 		}
 		footprintWkt = newStr.substr(1);
@@ -49,7 +61,7 @@ function validatePolygon(footprintWktInput){
 	if(footprintWkt.indexOf(",") > -1){
 		var firstSet = footprintWkt.substr(0,footprintWkt.indexOf(","));
 		var lastSet = footprintWkt.substr(footprintWkt.lastIndexOf(",")+1);
-		if(firstSet != lastSet) footprintWkt = footprintWkt + ", " + firstSet;
+		if(firstSet != lastSet) footprintWkt = footprintWkt + "," + firstSet;
 	}
 	footprintWkt = footprintWkt.trim();
 	if(footprintWkt != "") footprintWkt = "POLYGON (("+footprintWkt+"))";
@@ -60,17 +72,18 @@ function validatePoints(footprintWkt, switchPoints){
 	if(footprintWkt.substring(0,2) == "[{") return footprintWkt;
 	var retStr = "";
 	var strArr = footprintWkt.split(",");
+	if(!switchPoints){
+		if(parseInt(Math.abs(strArr[0].substring(0,strArr[0].indexOf(" ")).trim())) > 90) switchPoints = true;
+	}
 	for(var i=0; i < strArr.length; i++){
 		var xy = strArr[i].trim().split(" ");
-		if(!switchPoints && i == 0){
-			if(parseInt(Math.abs(xy[0])) > 90) switchPoints = true;
-		}
-		if(switchPoints){
-			retStr = retStr + ", " + parseFloat(xy[1]).toFixed(6) + " " + parseFloat(xy[0]).toFixed(6);
-		}
-		else{
-			retStr = footprintWkt;
-			break;
+		if(i<1 || strArr[i-1].trim() != strArr[i].trim()){
+			if(switchPoints){
+				retStr = retStr + "," + parseFloat(xy[1]).toFixed(6) + " " + parseFloat(xy[0]).toFixed(6);
+			}
+			else{
+				retStr = retStr + "," + parseFloat(xy[0]).toFixed(6) + " " + parseFloat(xy[1]).toFixed(6);
+			}
 		}
 	}
 	if(retStr.substr(0,1) == ",") retStr = retStr.substr(1);
@@ -78,6 +91,7 @@ function validatePoints(footprintWkt, switchPoints){
 }
 
 function trimPolygon(footprintWkt){
+	footprintWkt = footprintWkt.trim();
 	if(footprintWkt != ""){
 		if(footprintWkt.substring(0,10) == "POLYGON ((") footprintWkt = footprintWkt.slice(10,-2);
 		if(footprintWkt.substring(0,9) == "POLYGON((") footprintWkt = footprintWkt.slice(9,-2);
