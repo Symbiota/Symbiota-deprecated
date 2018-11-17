1e43f86c4db0a23d8fd11f571ba5017c2e3f3205
Tom Kerns <tom@tundratech.net>
2018-11-16 19:21:19 -0900

    Start of garden page taxa tab template to make it easier for Linda to create consistent garden tab content.

---
+++ garden/garden-tab-content-template.php
@@ -0,0 +1,87 @@
+<style>
+	ul.plant-facts, ul.plant-facts ul {
+		list-style: none;
+	}
+	.tab-header-wrapper {
+		display: flex;
+	}
+	.tab-header-wrapper .tab-printer-friendly {
+		margin-left: auto;
+		width: 150px;
+		display: block;
+		padding: 5px;
+		border: 1px solid darkred;
+		text-decoration: none;
+	}
+	.tab-images {
+		display: flex;
+		flex-wrap: wrap;
+
+	}
+	.tab-images figure {
+		width: 110px;
+		padding: 0;
+		margin: 5px;
+	}
+</style>
+<div class="garden-tab-wrapper">
+	<div class="tab-header-wrapper">
+		<div class="tab-icons">icons</div>
+		<a class="tab-printer-friendly" href="#">Printer-friendly-view</a>
+	</div>
+	<p>A favorite for hummingbirds and butterflies, red-flowering currant has tubular pink to red
+		flowers that, along with their leaves, produce a bright scent in early spring. Blue-black
+		berries develop in summer and are edible but tart. Stems are thornless unlike many of the
+		shrub’s relatives, and sport colorful autumn foliage.</p>
+	<ul class="plant-facts">
+		<li><strong>Plant Facts</strong>
+			<ul>
+				<li><strong>Plant type:</strong> deciduous shrub</li>
+				<li><strong>Size at maturity:</strong> 7-10’ high; 6-10’ wide</li>
+				<li><strong>Flower color:</strong> pink</li>
+				<li><strong>Bloom time:</strong> Jan – July</li>
+				<li><strong>Light:</strong> partial shade to full sun</li>
+				<li><strong>Moisture:</strong> moist to drought tolerant; moderate summer water</li>
+					<li><strong>Wildlife support:</strong> butterfly nectar source, beneficial insects, larval food source,
+					hummingbirds, pollinators</li>
+			</ul>
+		</li>
+		<li>
+			<ul><strong>Growth and Maintenance</strong>
+				<li><strong>Ease of cultivation:</strong> easy</li>
+				<li><strong>Spreads vigorously:</strong> no</li>
+				<li><strong>Landscape uses:</strong> erosion control, hedgerow, rock garden, thicket-forming,
+					woodland garden</li>
+				<li><strong>Other cultivation factors:</strong> well-drained soils</li>
+				<li><strong>Plant behavior:</strong> runners (rhizomes, stolons, etc.)</li>
+				<li><strong>Propagation:</strong> seed after stratification</li>
+			</ul>
+		</li>
+		<li>
+			<ul><strong>Commercial Availability:</strong> high
+				<li>We are looking forward to presenting the businesses that sell this plant. Contact us
+					if you’re interested in helping to develop this resource!</li>
+			</ul>
+		</li>
+	</ul>
+
+	<strong>Explore the garden collections containing red-flowering currant</strong>
+	<div class="tab-images">
+		<figure>
+			<img src="https://picsum.photos/100" alt="plantname">
+			<figcaption>plant name</figcaption>
+		</figure>
+		<figure>
+			<img src="https://picsum.photos/100" alt="plantname">
+			<figcaption>plant name</figcaption>
+		</figure>
+		<figure>
+			<img src="https://picsum.photos/100" alt="plantname">
+			<figcaption>plant name</figcaption>
+		</figure>
+		<figure>
+			<img src="https://picsum.photos/100" alt="plantname">
+			<figcaption>plant name</figcaption>
+		</figure>
+	</div>
+</div>
