70b96ed45a3b21a6fb1a0340c0711718e01aafdd
Tom Kerns <tom@tundratech.net>
2019-01-15 20:46:04 -0900

    Workaround for php 5.4 for garden search class.

--- classes/GardenSearchManager.php
+++ classes/GardenSearchManager.php
@@ -1,5 +1,31 @@
 <?php
 include_once($SERVER_ROOT.'/config/dbconnection.php');
+if (! function_exists('array_column')) {
+    function array_column(array $input, $columnKey, $indexKey = null) {
+        $array = array();
+        foreach ($input as $value) {
+            if ( !array_key_exists($columnKey, $value)) {
+                trigger_error("Key \"$columnKey\" does not exist in array");
+                return false;
+            }
+            if (is_null($indexKey)) {
+                $array[] = $value[$columnKey];
+            }
+            else {
+                if ( !array_key_exists($indexKey, $value)) {
+                    trigger_error("Key \"$indexKey\" does not exist in array");
+                    return false;
+                }
+                if ( ! is_scalar($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not contain scalar value");
+                    return false;
+                }
+                $array[$value[$indexKey]] = $value[$columnKey];
+            }
+        }
+        return $array;
+    }
+}
 
 class GardenSearchManager {
 
