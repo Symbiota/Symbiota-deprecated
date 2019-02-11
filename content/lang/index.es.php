d0baad534cd31033b3a8c159fabc7644b435df88
Augusto Basante <august_basante@yahoo.com>
2019-02-11 14:15:18 -0500

    Variables de lenguaje

---
+++ content/lang/index.es.php
@@ -0,0 +1,18 @@
+<?php
+/*
+------------------
+Language: Espa�ol (Spanish)
+------------------
+*/
+
+//Various plugin variables
+$LANG['QSEARCH_SEARCH'] = 'B&uacute;squeda de especies';
+$LANG['QSEARCH_SEARCH_BUTTON'] = 'B&uacute;squeda';
+
+//Slideshow plugin variables
+$LANG['HIDE_CAPTION'] = 'OCULTAR SUBT&Iacute;TULO';
+$LANG['SHOW_CAPTION'] = 'MOSTRAR T&Iacute;TULO';
+$LANG['IMAGE_BY'] = 'Imagen por';
+$LANG['COURTESY_OF'] = 'Cortes&iacute;a de';
+$LANG['HELP'] = 'AYUDA';
+?>
