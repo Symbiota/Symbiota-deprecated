ebb6b8848bde7599025b1c409d0e6c770a554982
Roger Leigh <rleigh@northcountry.com>
2020-02-28 17:39:49 -0800

    links page

--- pages/links.php
+++ pages/links.php
@@ -20,65 +20,93 @@ header( "Content-Type: text/html; charset=" . $charset );
 include( $serverRoot . "/header.php" );
 ?>
 
-<!-- if you need a full width column, just put it outside of .inner-content -->
-<!-- .inner-content makes a column max width 1100px, centered in the viewport -->
-<div class="inner-content links-page">
-    <!-- place static page content here. -->
-    <h1>Recommended online resources</h1>
+<div id="info-page">
+    <section id="titlebackground" style="background-image: url('/images/header/h1blueberry.png');">
+        <div class="inner-content">
+            <h1>Recommended online resources</h1>
+        </div>
+    </section>
+    <section>
+        <!-- if you need a full width column, just put it outside of .inner-content -->
+        <!-- .inner-content makes a column max width 1100px, centered in the viewport -->
+        <div class="inner-content links-page">
+            <!-- place static page content here. -->
+            <h2>Oregon Plant Data</h2>
+            <p><a target="_blank" href="https://bpp.oregonstate.edu/herbarium">Oregon State University Herbarium</a></p>
+            <p><a target="_blank" href="https://oregondigital.org/sets/herbarium">OSU Herbarium Type Specimens</a>
+                <span class="link-details">Images of the OSU Herbarium type specimens and their published descriptions</span></p>
+            <p><a target="_blank" href="http://plant-clinic.bpp.oregonstate.edu/plant-weed-identification/">OSU Plant and Weed Identification Service</a></p>
+            <p><a target="_blank" href="https://www.oregon.gov/ODA/programs/Weeds/Pages/Default.aspx">Oregon Dept. Agriculture (ODA) Weeds and WeedMapper</a></p>
+            <p><a target="_blank" href="https://www.oregon.gov/ODA/programs/PlantConservation/Pages/Default.aspx">ODA Plant Conservation</a></p>
+            <p><a target="_blank" href="https://inr.oregonstate.edu/orbic/rare-species/rare-species-oregon-publications">Rare, Threatened & Endangered Species of Oregon</a>
+                <span class="link-details">Oregon Biodiversity Information Center, OSU Institute for Natural Resources</span></p>
+            <p><a target="_blank" href="http://www.carexworkinggroup.com/index.html">Carex Working Group</a></p>
+            <p><a target="_blank" href="https://sites.google.com/site/orimapresources/">Oregon iMapInvasives</a>
+                <span class="link-details">GIS-based reporting and querying tool</p>
+            <p><a target="_blank" href="http://oregonexplorer.info/"> Oregon Explorer</a>
+                <span class="link-details">Natural Resources Digital Library, OSU Institute for Natural Resources</span></p>
 
-    <h2>Oregon Plant Data</h2>
-    <p>	<a target="_blank" href="http://oregonstate.edu/dept/botany/herbarium/">Oregon State University Herbarium</a><br>
-        <a target="_blank" href="https://oregondigital.org/sets/herbarium">OSU Herbarium Type Specimens</a> &nbsp;&nbsp;&nbsp;   Images of the OSU Herbarium type specimens and their published descriptions<br>
-        <a target="_blank" href="http://plant-clinic.bpp.oregonstate.edu/plant-weed-identification/">OSU Plant Identification Service</a><br>
-        <a target="_blank" href="https://www.oregon.gov/ODA/programs/Weeds/Pages/Default.aspx">ODA Weeds and WeedMapper</a><br>
-        <a target="_blank" href="https://www.oregon.gov/ODA/programs/PlantConservation/Pages/Default.aspx">ODA Plant Conservation</a><br>
-        <a target="_blank" href="https://inr.oregonstate.edu/orbic/rare-species/rare-species-oregon-publications">Rare, Threatened & Endangered Species of Oregon</a>  &nbsp;&nbsp;&nbsp;   Oregon Biodiversity Information Center, OSU Institute for Natural Resources<br>
-        <a target="_blank" href="https://sites.google.com/site/orimapresources/">Oregon iMapInvasives</a> &nbsp;&nbsp;&nbsp;   GIS-based reporting and querying tool<br>
-        <a target="_blank" href="http://oregonexplorer.info/"> Oregon Explorer</a> &nbsp;&nbsp;&nbsp;  Natural Resources Digital Library, OSU Institute for Natural Resources<br>
-        <a target="_blank" href="http://www.carexworkinggroup.com/index.html">Carex Working Group</a></p>
+            <h2>Regional, National</h2>
+            <p><a target="_blank" href="https://plants.sc.egov.usda.gov/java/">USDA PLANTS Database</a></p>
+            <p>	<a target="_blank" href="https://www.fs.usda.gov/main/r6/plants-animals/plants">U.S. Forest Service Region 6 Plants</a></p>
+            <p><a target="_blank" href="http://ucjeps.berkeley.edu/interchange.html">the Jepson Interchange</a>
+                <span class="link-details">California floristics data from the Jepson Herbarium, Univ. California Berkeley</span></p>
+            <p><a target="_blank" href="http://www.pnwherbaria.org/">Consortium of Pacific Northwest Herbaria</a>
+                <span class="link-details">Aggregation of herbarium specimens from the Pacific Northwest</span></p>
+            <p><a target="_blank" href="http://intermountainbiota.org/portal/index.php">Intermountain Regional Herbarium Network</a></p>
+            <p><a target="_blank" href="http://ibis.geog.ubc.ca/biodiversity/eflora/">E-flora BC</a>
+                <span class="link-details">Electronic atlas of the flora of British Columbia</span></p>
+            <p><a target="_blank" href="http://efloras.org/flora_page.aspx?flora_id=1">Flora of North America eFlora</a></p>
 
-    <h2>Regional, National</h2>
-    <p>	<a target="_blank" href="https://www.fs.usda.gov/main/r6/plants-animals/plants">U.S. Forest Service Plants</a><br>
-        <a target="_blank" href="http://ucjeps.berkeley.edu/interchange.html">the Jepson Interchange</a>  &nbsp;&nbsp;&nbsp;  California floristics data from the Jepson Herbarium, Univ. California Berkeley<br>
-        <a target="_blank" href="http://www.pnwherbaria.org/">Consortium of Pacific Northwest Herbaria</a>  &nbsp;&nbsp;&nbsp;  Aggregation of herbarium specimens from the Pacific Northwest<br>
-        <a target="_blank" href="http://intermountainbiota.org/portal/index.php">Intermountain Regional Herbarium Network</a><br>
-        <a target="_blank" href="http://ibis.geog.ubc.ca/biodiversity/eflora/">E-flora BC</a>  &nbsp;&nbsp;&nbsp;  Electronic atlas of the flora of British Columbia<br>
-        <a target="_blank" href="http://efloras.org/flora_page.aspx?flora_id=1">Flora of North America eFlora</a><br>
-        <a target="_blank" href="https://plants.sc.egov.usda.gov/java/">USDA PLANTS Database</a></p>
+            <h2>Biodiversity Data Sharing</h2>
+            <p><a target="_blank" href="http://symbiota.org/docs/">Symbiota</a>
+                <span class="link-details">An open source content management system for curating specimen- and observation-based biodiversity data</span></p>
+             <p><a target="_blank" href="https://www.gbif.org/">GBIF Global Biodiversity Information Facility</a>
+                <span class="link-details">Free and open access to biodiversity data</p>
+             <p><a target="_blank" href="https://www.idigbio.org/">iDigBio</a>
+                <span class="link-details">Data and images of millions of biological specimens on the web</p>
 
-    <h2>Biodiversity Data Sharing</h2>
-    <p><a target="_blank" href="http://symbiota.org/docs/">Symbiota</a> &nbsp;&nbsp;&nbsp;   An open source content management system for curating specimen- and observation-based biodiversity data<br>
-        <a target="_blank" href="https://www.gbif.org/">GBIF Global Biodiversity Information Facility</a> &nbsp;&nbsp;&nbsp;   Free and open access to biodiversity data<br>
-        <a target="_blank" href="https://www.idigbio.org/">iDigBio</a> &nbsp;&nbsp;&nbsp; Data and images of millions of biological specimens on the web<br></p>
+            <h2>Photo Collections</h2>
+            <p><a target="_blank" href="http://www.botany.hawaii.edu/faculty/carr/ofp/ofp_index.htm">Dr. Gerald Carr’s Oregon plant images</a></p>
+            <p><a target="_blank" href="http://biology.burke.washington.edu/herbarium/imagecollection.php">University of Washington Herbarium Image Collection</a>
+                <span class="link-details">Burke Museum</span></p>
+            <p><a target="_blank" href="https://calphotos.berkeley.edu/flora/">CalPhotos: Plants</a></p>
 
-    <h2>Photo Collections</h2>
-    <p><a target="_blank" href="http://www.botany.hawaii.edu/faculty/carr/ofp/ofp_index.htm">Dr. Gerald Carr’s Oregon plant images</a><br>
-        <a target="_blank" href="http://biology.burke.washington.edu/herbarium/imagecollection.php">University of Washington Herbarium Image Collection</a> &nbsp;&nbsp;&nbsp;   Burke Museum<br>
-        <a target="_blank" href="https://calphotos.berkeley.edu/flora/">CalPhotos: Plants</a> </p>
+            <h2>Native Plant Societies</h2>
+            <p><a target="_blank" href="http://www.npsoregon.org/">Oregon</a></p>
+            <p><a target="_blank" href="https://www.wnps.org/">Washington</a><br>
+            <p><a target="_blank" href="https://www.cnps.org/">California</a></p>
+            <p><a target="_blank" href="https://idahonativeplants.org/">Idaho</a></p>
+            <p><a target="_blank" href="https://www.nvnps.org/">Nevada</a></p>
 
-    <h2>Native Plant Societies</h2>
-    <p>	<a target="_blank" href="http://www.npsoregon.org/">Oregon</a><br>
-        <a target="_blank" href="https://www.wnps.org/">Washington</a><br>
-        <a target="_blank" href="https://www.cnps.org/">California</a><br>
-        <a target="_blank" href="https://idahonativeplants.org/">Idaho</a><br>
-        <a target="_blank" href="https://www.nvnps.org/">Nevada</a> </p>
+            <h2>Gardening with Native Plants</h2>
+            <p><a target="_blank" href="http://www.npsoregon.org/landscaping1.html">Using Native Plants for Gardening</a>
+                <span class="link-details">Native Plant Society of Oregon</span></p>
+            <p><a target="_blank" href="http://emerald.npsoregon.org/GardeningWithNatives.html">Gardening with Native Plants</a>
+                <span class="link-details">Species lists for the southern Willamette Valley. Emerald Chapter, Native Plant Society of Oregon</span></p>
+            <p><a target="_blank" href="http://www.plantnative.org/how_intro.htm">How to Naturescape</a>
+                <span class="link-details">PlantNative.org</span></p>
+            <p><a target="_blank" href="https://catalog.extension.oregonstate.edu/ec1577">Gardening with Native Plants West of the Cascades</a
+                <span class="link-details">Linda McMahan, OSU Extension</span></p>
+            <p><a target="_blank" href="http://www.nwplants.com/index.html">The Wild Garden</a>
+                <span class="link-details">Hansen’s Northwest Native Plant Database</span></p>
 
-    <h2>Gardening with Native Plants</h2>
-    <p><a target="_blank" href="http://www.npsoregon.org/landscaping1.html">Using Native Plants for Gardening</a>  &nbsp;&nbsp;&nbsp;  Native Plant Society of Oregon<br>
-        <a target="_blank" href="http://www.plantnative.org/how_intro.htm">How to Naturescape</a> &nbsp;&nbsp;&nbsp;   PlantNative.org<br>
-        <a target="_blank" href="http://emerald.npsoregon.org/GardeningWithNatives.html">Gardening with Native Plants</a>  &nbsp;&nbsp;&nbsp;  species lists for the southern Willamette Valley. Emerald Chapter, Native Plant Society of Oregon<br>
-        <a target="_blank" href="http://www.nwplants.com/index.html">the Wild Garden</a> &nbsp;&nbsp;&nbsp; Hansen’s Northwest Native Plant Database</p>
+            <h2>Habitat Restoration, Working Agricultural Lands</h2>
+            <p><a target="_blank" href="http://www.heritageseedlings.com/habitat-restoration">Habitat Restoration in the Willamette Valley</a>
+                <span class="link-details">Heritage Seedlings</span></p>
+            <p><a target="_blank" href="https://cascadiaprairieoak.org/wp-content/uploads/2018/10/Hamman_Restoring-Native-Diversity_Working-Lands_CPOP2018.pdf">Restoring native diversity in working lands</a>
+                 <span class="link-details">Cascadia Prairie Oak Partnership</span></p>
+            <p><a target="_blank" href="https://cascadiaprairieoak.org/resources/wet-prairie-guide">Cascadia Prairie Oak Partnership</a>
+                <span class="link-details">Wetland Prairie Restoration Guide</span></p>
 
-    <h2>Habitat Restoration, Working Agricultural Lands</h2>
-    <p>	<a target="_blank" href="http://www.heritageseedlings.com/habitat-restoration">Habitat Restoration in the Willamette Valley</a>    &nbsp;&nbsp;&nbsp;Heritage Seedlings<br>
-        <a target="_blank" href="https://cascadiaprairieoak.org/wp-content/uploads/2018/10/Hamman_Restoring-Native-Diversity_Working-Lands_CPOP2018.pdf">Restoring native diversity in working lands</a>  &nbsp;&nbsp;&nbsp;  Cascadia Prairie Oak Partnership</p>
+            <h2>Connect with OregonFlora on Social Media</h2>
+            <p>
+                <a target="_blank" href="https://www.facebook.com/OregonFlora/"><img src="/pages/images/icon-facebook.png" width=80px alt="Facebook" title="Facebook"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a target="_blank" href="https://www.instagram.com/oregonflora/?hl=en"><img src="/pages/images/icon-instagram.png" width=80px alt="Instagram" title="Instagram"</a></p>
 
-    <h2>Connect with OregonFlora on Social Media</h2>
-    <p> <a target="_blank" href="https://www.facebook.com/OregonFlora/">on Facebook</a><br>
-        <a target="_blank" href="https://www.instagram.com/oregonflora/?hl=en">on Instagram</a></p>
-
-</div> <!-- .inner-content -->
-
+        </div> <!-- .inner-content -->
+    </section>
+</div>
 <?php
 include( $serverRoot . "/footer.php" );
 ?>
