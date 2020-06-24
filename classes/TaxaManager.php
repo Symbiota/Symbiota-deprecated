6c7935c81308fb96f224d98f3fc4bb3ae04f55f2
Arthur Parker <alp9000@gmail.com>
2020-06-24 15:57:13 -0700

    tdbid and srouce

--- classes/TaxaManager.php
+++ classes/TaxaManager.php
@@ -271,7 +271,7 @@ class TaxaManager {
   	if ($tid) {
 			$em = SymbosuEntityManager::getEntityManager();
 			$rsArr = $em->createQueryBuilder()
-				->select(["ts.tid, tdb.tdbid, tdb.language"])#, tdb.source, tdb.sourceurl, tdb.caption, tds.tdsid, tds.heading, tds.statement, tds.displayheader
+				->select(["ts.tid, tdb.tdbid, tdb.source"])#, tdb.language, tdb.source, tdb.sourceurl, tdb.caption, tds.tdsid, tds.heading, tds.statement, tds.displayheader
 				->from("Taxstatus", "ts")
 				->innerJoin("Taxadescrblock", "tdb", "WITH", "ts.tid = tdb.tid")
 				#->innerJoin("Taxadescrstmts", "tds", "WITH", "tds.tdbid = tdb.tdbid")
