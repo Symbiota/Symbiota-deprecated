ee58a2a9aac4a979c583e4b2ffd2abf58f2bcdb4
Arthur Parker <alp9000@gmail.com>
2020-06-24 15:52:31 -0700

    no tdbid no caption

--- classes/TaxaManager.php
+++ classes/TaxaManager.php
@@ -271,7 +271,7 @@ class TaxaManager {
   	if ($tid) {
 			$em = SymbosuEntityManager::getEntityManager();
 			$rsArr = $em->createQueryBuilder()
-				->select(["ts.tid, tdb.tdbid, tdb.caption, tdb.source, tdb.sourceurl, tdb.language"])#, tds.tdsid, tds.heading, tds.statement, tds.displayheader
+				->select(["ts.tid, tdb.tdbid, tdb.caption, tdb.source, tdb.sourceurl, tdb.language"])#, tdb.tdbid, tdb.caption, tds.tdsid, tds.heading, tds.statement, tds.displayheader
 				->from("Taxstatus", "ts")
 				->innerJoin("Taxadescrblock", "tdb", "WITH", "ts.tid = tdb.tid")
 				#->innerJoin("Taxadescrstmts", "tds", "WITH", "tds.tdbid = tdb.tdbid")
