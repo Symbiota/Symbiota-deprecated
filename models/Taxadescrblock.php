fe3755847cf6227b97220a2af620c3584a8d7101
Arthur Parker <alp9000@gmail.com>
2020-06-24 16:45:08 -0700

    min with caption

--- models/Taxadescrblock.php
+++ models/Taxadescrblock.php
@@ -41,46 +41,7 @@ class Taxadescrblock
    * @ORM\Column(name="caption")
    */
   private $caption;
-  
-  /**
-   * @var string|null
-   *
-   * @ORM\Column(name="source")
-   */
-  private $source;
-  
-  /**
-   * @var string|null
-   *
-   * @ORM\Column(name="sourceurl")
-   */
-  private $sourceurl;
-  
-  /**
-   * @var string|null
-   *
-   * @ORM\Column(name="language")
-   */
-  private $language;
-  
-  /**
-   * @var string|null
-   *
-   * @ORM\Column(name="displaylevel")
-   */
-  private $displaylevel;
-
-  public function getTdbid() {
-    return $this->tdbid;
-  }
-
-  public function getTid() {
-    return $this->tid;
-  }
-  
-  public function getDisplayLevel() {
-    return $this->displaylevel;
-  }
+ 
 }
 
 ?>
