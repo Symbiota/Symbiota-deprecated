45a197e1a216a001d213d390838ab2a007873cb2
Arthur Parker <alp9000@gmail.com>
2020-06-24 11:28:15 -0700

    Again removing desc and spp from Taxamanager

--- classes/TaxaManager.php
+++ classes/TaxaManager.php
@@ -63,10 +63,10 @@ class TaxaManager {
       $this->images = TaxaManager::populateImages($this->getTid());
       $this->characteristics = TaxaManager::populateCharacteristics($this->getTid());
       $this->checklists = TaxaManager::populateChecklists($this->getTid());
-      $this->descriptions = $this->populateDescriptions($this->getTid());
+      #$this->descriptions = $this->populateDescriptions($this->getTid());
       $this->gardenDescription = $this->populateGardenDescription($this->getTid());
       $this->populateTaxalinks($this->getTid());
-      $this->spp = $this->populateSpp($this->getTid());
+      #$this->spp = $this->populateSpp($this->getTid());
     } else {
       $this->model = null;
       $this->basename = '';
@@ -87,9 +87,9 @@ class TaxaManager {
     $newTaxa->images = TaxaManager::populateImages($model->getTid());
     $newTaxa->characteristics = TaxaManager::populateCharacteristics($model->getTid());
     $newTaxa->checklists = TaxaManager::populateChecklists($model->getTid());
-    $newTaxa->descriptions = $newTaxa->populateDescriptions($model->getTid());
+    #$newTaxa->descriptions = $newTaxa->populateDescriptions($model->getTid());
     $newTaxa->gardenDescription = $newTaxa->populateGardenDescription($model->getTid());
-    $newTaxa->spp = $newTaxa->populateSpp($model->getTid());
+    #$newTaxa->spp = $newTaxa->populateSpp($model->getTid());
     return $newTaxa;
   }
   
