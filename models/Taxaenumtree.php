6384105b3534e352268b5d8160520f92eb02c9d4
Arthur Parker <alp9000@gmail.com>
2020-06-25 12:42:00 -0700

    taxauthid only

--- models/Taxaenumtree.php
+++ models/Taxaenumtree.php
@@ -7,41 +7,12 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Taxaenumtree
  *
- * @ORM\Table(name="taxaenumtree", uniqueConstraints={@ORM\UniqueConstraint(name="PRIMARY", columns={"tid", "taxauthid", "parenttid"})}, indexes={@ORM\Index(name="FK_tet_taxa2", columns={"parenttid"}), @ORM\Index(name="FK_tet_taxa", columns={"tid"}), @ORM\Index(name="FK_tet_taxauth", columns={"taxauthid"})})
+ * @ORM\Table(name="taxaenumtree")
  * @ORM\Entity
  */
 class Taxaenumtree
 {
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="initialtimestamp", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
-     */
-    private $initialtimestamp = 'CURRENT_TIMESTAMP';
 
-    /**
-     * @var \Taxa
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     * @ORM\OneToOne(targetEntity="Taxa")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="tid", referencedColumnName="TID")
-     * })
-     */
-    private $tid;
-
-    /**
-     * @var \Taxa
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     * @ORM\OneToOne(targetEntity="Taxa")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="parenttid", referencedColumnName="TID")
-     * })
-     */
-    private $parenttid;
 
     /**
      * @var integer
