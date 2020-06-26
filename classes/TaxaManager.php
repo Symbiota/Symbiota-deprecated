e25bf0b7d4daa2b2fc116d29812102cb1bd5efa9
Arthur Parker <alp9000@gmail.com>
2020-06-26 14:18:22 -0700

    working on taxadescrblock

--- classes/TaxaManager.php
+++ classes/TaxaManager.php
@@ -63,7 +63,7 @@ class TaxaManager {
       $this->images = TaxaManager::populateImages($this->getTid());
       $this->characteristics = TaxaManager::populateCharacteristics($this->getTid());
       $this->checklists = TaxaManager::populateChecklists($this->getTid());
-      #$this->descriptions = $this->populateDescriptions($this->getTid());
+      $this->descriptions = $this->populateDescriptions($this->getTid());
       $this->gardenDescription = $this->populateGardenDescription($this->getTid());
       $this->populateTaxalinks($this->getTid());
       $this->spp = $this->populateSpp($this->getTid());
@@ -87,7 +87,7 @@ class TaxaManager {
     $newTaxa->images = TaxaManager::populateImages($model->getTid());
     $newTaxa->characteristics = TaxaManager::populateCharacteristics($model->getTid());
     $newTaxa->checklists = TaxaManager::populateChecklists($model->getTid());
-    #$newTaxa->descriptions = $newTaxa->populateDescriptions($model->getTid());
+    $newTaxa->descriptions = $newTaxa->populateDescriptions($model->getTid());
     $newTaxa->gardenDescription = $newTaxa->populateGardenDescription($model->getTid());
     $newTaxa->spp = $newTaxa->populateSpp($model->getTid());
     return $newTaxa;
