b7a905d5931a01a74ab6329243015fed641ae978
Arthur Parker <alp9000@gmail.com>
2020-09-09 12:12:38 -0700

    Spatial test5

--- spatial/rpc/SOLRConnector.php
+++ spatial/rpc/SOLRConnector.php
@@ -12,79 +12,89 @@ if(isset($_REQUEST["start"])) $pArr["start"] = $_REQUEST["start"];
 if(isset($_REQUEST["fl"])) $pArr["fl"] = $_REQUEST["fl"];
 if(isset($_REQUEST["wt"])) $pArr["wt"] = $_REQUEST["wt"];
 
+$canReadRareSpp = false;
+if($GLOBALS['USER_RIGHTS']){
+		if($GLOBALS['IS_ADMIN'] || array_key_exists("CollAdmin", $GLOBALS['USER_RIGHTS']) || array_key_exists("RareSppAdmin", $GLOBALS['USER_RIGHTS']) || array_key_exists("RareSppReadAll", $GLOBALS['USER_RIGHTS'])){
+				$canReadRareSpp = true;
+		}
+}
+
 $solrManager = new SOLRManager();
-#$pArr["q"] = $solrManager->checkQuerySecurity($pArr["q"]);
+$origQ = $pArr["q"];
+$secureQ = $solrManager->checkQuerySecurity($pArr["q"]);
+
+#get protected results
+$pArr["q"] = $secureQ;
 
 if($pArr["wt"] == 'geojson'){
-    $pArr["geojson.field"] = 'geo';
-    $pArr["omitHeader"] = 'true';
+		$pArr["geojson.field"] = 'geo';
+		$pArr["omitHeader"] = 'true';
 }
-
-/*$testURL = $SOLR_URL.'/select?';
-foreach($pArr as $key => $value){
-    $testURL .= $key.'='.$value.'&';
-}
-echo substr($testURL, 0, -1);*/
 
 $headers = array(
-    'Content-Type: application/x-www-form-urlencoded',
-    'Accept: application/json',
-    'Cache-Control: no-cache',
-    'Pragma: no-cache',
-    'Content-Length: '.strlen(http_build_query($pArr))
+		'Content-Type: application/x-www-form-urlencoded',
+		'Accept: application/json',
+		'Cache-Control: no-cache',
+		'Pragma: no-cache',
+		'Content-Length: '.strlen(http_build_query($pArr))
 );
 
 $ch = curl_init();
 
 $options = array(
-    CURLOPT_URL => $SOLR_URL.'/select',
-    CURLOPT_POST => true,
-    CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_TIMEOUT => 90,
-    CURLOPT_POSTFIELDS => http_build_query($pArr),
-    CURLOPT_RETURNTRANSFER => true
+		CURLOPT_URL => $SOLR_URL.'/select',
+		CURLOPT_POST => true,
+		CURLOPT_HTTPHEADER => $headers,
+		CURLOPT_TIMEOUT => 90,
+		CURLOPT_POSTFIELDS => http_build_query($pArr),
+		CURLOPT_RETURNTRANSFER => true
 );
 curl_setopt_array($ch, $options);
-$result = curl_exec($ch);
+$secureResult = curl_exec($ch);
 curl_close($ch);
-echo $result;
+$result = $secureResult;
+
+$res = json_decode($result);
+var_dump($res);
+/*
+if (!$canReadRareSpp)
+	$pArr["q"] = $origQ;
+
+	if($pArr["wt"] == 'geojson'){
+			$pArr["geojson.field"] = 'geo';
+			$pArr["omitHeader"] = 'true';
+	}
 
 
-$pArr["q"] = $solrManager->checkQuerySecurity($pArr["q"]);
+	$headers = array(
+			'Content-Type: application/x-www-form-urlencoded',
+			'Accept: application/json',
+			'Cache-Control: no-cache',
+			'Pragma: no-cache',
+			'Content-Length: '.strlen(http_build_query($pArr))
+	);
 
-if($pArr["wt"] == 'geojson'){
-    $pArr["geojson.field"] = 'geo';
-    $pArr["omitHeader"] = 'true';
+	$ch = curl_init();
+
+	$options = array(
+			CURLOPT_URL => $SOLR_URL.'/select',
+			CURLOPT_POST => true,
+			CURLOPT_HTTPHEADER => $headers,
+			CURLOPT_TIMEOUT => 90,
+			CURLOPT_POSTFIELDS => http_build_query($pArr),
+			CURLOPT_RETURNTRANSFER => true
+	);
+	curl_setopt_array($ch, $options);
+	$partialResult = curl_exec($ch);
+	curl_close($ch);
+	
+	
+	
 }
+*/
 
-/*$testURL = $SOLR_URL.'/select?';
-foreach($pArr as $key => $value){
-    $testURL .= $key.'='.$value.'&';
-}
-echo substr($testURL, 0, -1);*/
 
-$headers = array(
-    'Content-Type: application/x-www-form-urlencoded',
-    'Accept: application/json',
-    'Cache-Control: no-cache',
-    'Pragma: no-cache',
-    'Content-Length: '.strlen(http_build_query($pArr))
-);
-
-$ch = curl_init();
-
-$options = array(
-    CURLOPT_URL => $SOLR_URL.'/select',
-    CURLOPT_POST => true,
-    CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_TIMEOUT => 90,
-    CURLOPT_POSTFIELDS => http_build_query($pArr),
-    CURLOPT_RETURNTRANSFER => true
-);
-curl_setopt_array($ch, $options);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result;
+#echo $result;
 
 
 ?>
