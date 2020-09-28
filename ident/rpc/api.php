339a41adadb2c6f4c039e78d6bf4be7111795746
Arthur Parker <alp9000@gmail.com>
2020-09-28 12:49:13 -0700

    Inventory test10

--- ident/rpc/api.php
+++ ident/rpc/api.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 include_once("../../config/symbini.php");
 include_once("$SERVER_ROOT/config/SymbosuEntityManager.php");
