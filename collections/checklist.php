1b3ff24235c5177baf719ae600a3808f559b5585
Arthur Parker <alp9000@gmail.com>
2020-10-08 16:50:50 -0700

    rm test

--- collections/checklist.php
+++ collections/checklist.php
@@ -89,7 +89,6 @@ if($stArrCollJson || $stArrSearchJson){
 	<?php
 		echo '<div style="font-weight:bold;font-size:125%;">'.$LANG['TAXA_COUNT'].': '.$taxaCnt.'</div>';
 		$undFamilyArray = Array();
-		var_dump($checklistArr);
 		if(array_key_exists("undefined",$checklistArr)){
 			$undFamilyArray = $checklistArr["undefined"];
 			unset($checklistArr["undefined"]);
