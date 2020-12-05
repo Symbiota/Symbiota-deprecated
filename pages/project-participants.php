66591a4f7d4fba60da1ba32791e5dcedcd9c949a
Linda Hardison <53278295+lindahardison@users.noreply.github.com>
2020-12-05 00:53:27 -0800

    Text updates

--- pages/project-participants.php
+++ pages/project-participants.php
@@ -75,11 +75,13 @@ include( $serverRoot . "/header.php" );
         <!-- place static page content here. -->
             <h2>Staff</h2>
 							<p>Linda Hardison, Director<br /><?php echo obfuscate("hardisol@science.oregonstate.edu") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 541-737-4338</p>
-							<p>Stephen Meyers, Taxonomic Director<br /><?php echo obfuscate("meyersst@science.oregonstate.edu") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 541-737-4338</p>
+							<p>Tanya Harvey, <em>Flora of Oregon</em> graphic designer<br /><?php echo obfuscate("tanya@westerncascades.com") ?></p>							
 							<p>Thea Jaster, Database manager, botanist<br /><?php echo obfuscate("jastert@science.oregonstate.edu") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 541-737-2445</p>
+							<p>Stephen Meyers, Taxonomic Director<br /><?php echo obfuscate("meyersst@science.oregonstate.edu") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 541-737-4338</p>
 							<p>Katie Mitchell, Database manager, botanist<br /><?php echo obfuscate("mitchelk@science.oregonstate.edu") ?></p>
 							<p>John Myers, <em>Flora of Oregon</em> principal illustrator<br /><?php echo obfuscate(" myersj8@oregonstate.edu") ?></p>
-							<p>Tanya Harvey, <em>Flora of Oregon</em> graphic designer<br /><?php echo obfuscate("tanya@westerncascades.com") ?></p>
+							
+							<br>
             <h2>Affiliates</h2>
 							<p>Dennis Albert</p>
 							<p>Jason Bradford</p>
@@ -89,11 +91,12 @@ include( $serverRoot . "/header.php" );
 							<p>Christian Feuillet</p>
 							<p>Richard Halse</p>
 							<p>George Kral</p>
+							<br>
             <h2>Students (2020)</h2>
-							<p>Rosa Arellanes</p>
 							<p>Mya Falk</p>
 							<p>Amadeo Ramos</p>
 							<p>Jessica Tuson</p>
+							<br>
             <h2>Advisory Council</h2>
 							<p>Lynda Boyer</p>
 							<p>Jason Bradford</p>
@@ -101,19 +104,22 @@ include( $serverRoot . "/header.php" );
 							<p>Will McClatchey</p>
 							<p>Joan Seevers</p>
 							<p>Robert Soreng</p>
+							<br>
             <h2>Project Partners</h2>
-							<p>The Native Plant Society of Oregon (NPSO) has been a sponsor of OregonFlora since the project&rsquo;s inception in 1994. The Society and its chapters provide financial support and promote the exchange of plant observation data and photographs.</p>
-							<p>Our close ties with the OSU Herbarium are mutually beneficial &mdash; the Herbarium excels as a dynamic resource with exceptional depth in its Oregon collections, and OregonFlora enhances their value with careful taxonomic analysis and digital presentation within the context of the OregonFlora tools.</p>
-							<p>Metro and its partners in the Adult Conservation Educators working group have led the development and support of the Grow Natives section of this website.</p>
+							<p>The <a href="http://npsoregon.org" target="_blank">Native Plant Society of Oregon (NPSO)</a> has been a sponsor of OregonFlora since the project&rsquo;s inception in 1994. The Society and its chapters provide financial support and promote the exchange of plant observation data and photographs.</p>
+							<p>Our close ties with the <a href="https://bpp.oregonstate.edu/herbarium" target"_blank">OSU Herbarium</a> are mutually beneficial &mdash; the Herbarium excels as a dynamic resource with exceptional depth in its Oregon collections, and OregonFlora enhances their value with careful taxonomic analysis and digital presentation within the context of the OregonFlora tools.</p>
+							<p><a href="https://oregonmetro.gov/garden" target="_blank">Metro</a> and its partners in the Northwest Adult Conservation Educators (NW-ACE) working group have led the development and support of the Grow Natives section of this website. These groups conceived and developed the foundation of this tool and have assisted in its launching as the Grow Natives section of our website.</p>
+							<p>Primary members/contributors of NW-ACE are: <a href="https://www.cleanwaterservices.org/" target="_blank">Clean Water Services</a>, <a href="https://emswcd.org" target="_blank">East Multnomah Soil & Water Conservation District</a>, <a href="https://tualatinswcd.org" target="_blank">Tualatin Soil & Water Conservation District</a>, and <a href="https://wmswcd.org"target="_blank">West Multnomah Soil & Water Conservation District</a>.</p>
 							<p>This website uses the Symbiota platform with customized modules. Our website is hosted at the College of Science Information Network (COSINe) at Oregon State University.</p>
 							<p>We collaborate and share information with numerous groups: academic institutions, federal agencies (Oregon/Washington Bureau of Land Management, US Forest Service), state organizations (Oregon Biodiversity Information Center), Metro, Native Plant Society of Oregon, and individuals.&nbsp;</p>
+							<br>
 						<h2>Key Supporters</h2>
 							<p>John and Betty Soreng Environmental Fund of the Oregon Community Foundation</p>
 							<p>Oregon/Washington Bureau of Land Management</p>
 							<p>Native Plant Society of Oregon</p>
 							<p>Metro (Portland Oregon)</p>
 							<p>Barbara J. Mendius Administrative Trust</p>
-							<p>USDA--Natural Resources Conservation Service (Oregon)</p>
+							<p>USDA&mdash;Natural Resources Conservation Service (Oregon)</p>
 							<p>Department of Botany &amp; Plant Pathology, Oregon State University</p>`
         </div> <!-- .inner-content -->
     </section>
