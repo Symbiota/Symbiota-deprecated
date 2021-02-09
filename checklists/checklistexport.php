9ec69f726df34506f3e431b2f6948209f6906b38
Arthur Parker <alp9000@gmail.com>
2021-02-09 13:51:57 -0800

    testing phpword21

--- checklists/checklistexport.php
+++ checklists/checklistexport.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include_once('../config/symbini.php');
+#include_once('../config/symbini.php');
 
 function exportChecklistToCSV($checklist) {
 	
